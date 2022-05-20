bebf2d556fe2bf8b1a5c7bf652065996f5097ac5
Shyam Mylai <34809629+shyam2520@users.noreply.github.com>
2022-05-20 06:46:13 +0530

    Time: 34 ms (5.09%), Space: 7.9 MB (13.17%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int res=0;
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),0));
+        // cout<<dp.size()<<" "<<dp[0].size()<<endl;
+        return dfs(grid,0,0,dp);
+        // return res;
+    }
+    
+    int  dfs(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& dp)
+    {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==1)
+            return 0;
+        if(i==grid.size()-1 && j==grid[0].size()-1) return 1;
+
+        // cout<<i<<" "<<j<<" "<<dp[i][j]<<endl;
+        
+        if(dp[i][j]) return dp[i][j];
+        
+        grid[i][j]=1;
+        
+        dp[i][j]=dfs(grid,i,j+1,dp) +dfs(grid,i+1,j,dp);
+        
+        grid[i][j]=0;
+        return dp[i][j];
+    }
+};
