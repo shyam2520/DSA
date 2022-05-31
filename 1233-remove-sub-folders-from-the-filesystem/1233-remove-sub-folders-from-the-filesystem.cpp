b9ce58abc5aa35f26d0ab816616175558f955817
Shyam Mylai <34809629+shyam2520@users.noreply.github.com>
2022-05-31 10:03:45 +0530

    Time: 284 ms (51.22%), Space: 43.8 MB (82.62%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -1,33 +1,14 @@
-//TC : O(N*(M*LOGN(N))
-//SC : O(N)
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        unordered_map<string,string> dict;
         sort(begin(folder),end(folder));
-        // for(auto& i: folder) cout<<i<<endl;
-        for(auto& name:folder)
+        vector<string> res;
+        res.push_back(folder[0]);
+        for(int i=1;i<folder.size();i++)
         {
-            // int r=name.length()-1;
-            
-            string temp=string(1,name[0]);
-            
-            for(int i=1;i<name.length();i++)
-            {
-                
-                if(name[i]=='/' && dict.count(temp))
-                    dict[name]=dict[temp];
-                temp+=name[i];
-            }
-            if(!dict.count(name))
-                dict[name]=name;
-
-        
+            int idx=folder[i].find(res[res.size()-1]+"/");
+            if(idx==string::npos || idx!=0) res.push_back(folder[i]);
         }
-        unordered_set<string> values;
-        for(auto& i:dict) values.insert(i.second);
-        
-        vector<string> res(begin(values),end(values));
         return res;
     }
 };
