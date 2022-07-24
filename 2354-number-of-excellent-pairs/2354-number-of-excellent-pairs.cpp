b254df37c1438c3b614fcf0103b4a91699765ec3
Shyam Mylai <34809629+shyam2520@users.noreply.github.com>
2022-07-24 14:18:09 +0530

    Time: 686 ms (50.00%), Space: 86.9 MB (50.00%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     long long countExcellentPairs(vector<int>& nums, int k) {
         long long ans=0;
-        unordered_map<int,unordered_set<int>> dict;
-        for(auto& i:nums)
+        unordered_set<int> n(begin(nums),end(nums));
+        unordered_map<int,int> dict;
+        for(auto& i:n)
         {
             int count=0;
             int temp=i;
-            while(i) 
+            while(temp) 
             {
-                count+=i&1;
-                i>>=1;
+                count+=temp&1;
+                temp>>=1;
             }
-            dict[count].insert(temp);
+            dict[count]++;
             // cout<<count<<" "<<dict[count].size()<<endl;;
             
         }
@@ -21,8 +22,8 @@ public:
         {
             for(int j=1;j<=30;j++)
             {
-                if(i+j<k || !dict[i].size() ||!dict[j].size()) continue;
-                ans+=dict[i].size()*dict[j].size();
+                if(i+j<k || !dict[i] ||!dict[j]) continue;
+                ans+=dict[i]*dict[j];
             }
         }
         return ans;
