7ad5a9b18741cbe395b9ed9469c2e1b850686e1f
Shyam Mylai <34809629+shyam2520@users.noreply.github.com>
2022-08-22 21:36:49 +0530

    Time: 3 ms (58.67%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0) return false;
-        while(!(n%4)) n/=4;
-        return n==1;
+        if(n<0 ) return false;
+        int ones=0,zeros=0;
+        while(n)
+        {
+            ones+=n&1;
+            // cout<<(n&1)<<endl;
+            zeros+=((n&1)==0)?1:0;
+            n>>=1;
+            // cout<<n<<endl;
+            // cout<<ones<<" "<<zeros<<endl;
+        }
+        return ones==1 && !(zeros%2);
     }
 };
