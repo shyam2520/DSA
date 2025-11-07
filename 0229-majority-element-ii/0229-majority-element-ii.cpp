d5d45a189e5a805fb47a86dacf95b0ac829889e2
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-07 12:46:39 -0500

    Time: 0 ms (100%), Space: 23.8 MB (46.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,11 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> dict;
-        int n = nums.size();
-        vector<int> res;
-        for(auto& i:nums) dict[i]++;
-        for(auto& i:dict) if(i.second>n/3) res.push_back(i.first);
-        return res;
+        int cnt1=0,cnt2=0,ele1=INT_MIN,ele2=INT_MIN; 
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && nums[i]!=ele2){
+                cnt1++;
+                ele1=nums[i];
+            } 
+            else if(cnt2==0 && nums[i]!=ele1){
+                cnt2++; 
+                ele2=nums[i];
+            } 
+            else if(nums[i]==ele1) cnt1++;
+            else if(nums[i]==ele2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        set<int> res;
+        cnt1=0,cnt2=0;
+        for(auto& i:nums){
+            if(i==ele1){
+                cnt1++;
+                if(cnt1>nums.size()/3) res.insert(ele1);
+            } 
+            if(i==ele2){
+                cnt2++;
+                if(cnt2>nums.size()/3) res.insert(ele2);
+            }
+        }
+        vector<int> ans(begin(res),end(res));
+        return ans;
+
     }
 };
