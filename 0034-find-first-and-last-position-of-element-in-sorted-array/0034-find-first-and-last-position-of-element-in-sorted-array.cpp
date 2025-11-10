0c719e64f0c1b15bce055c99c35ec8b4b5dc0e12
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-10 12:12:15 -0500

    Time: 0 ms (100%), Space: 17.5 MB (86.27%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int lower_bound(vector<int>& nums,int target){
-        int l=0,r=nums.size()-1; 
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1,lpos=nums.size();
         while(l<=r){
             int mid = l+(r-l)/2; 
-            if(nums[mid]<target) l=mid+1; 
+            if(nums[mid]<target) l=mid+1;
+            else{
+                lpos=mid;
+                r=mid-1;
+            }
+        }
+        if(lpos==nums.size() || nums[lpos]!=target) return {-1,-1};
+
+        l=lpos;r=nums.size()-1;int rpos=nums.size();
+        while(l<=r){
+            int mid = l+(r-l)/2; 
+            if(nums[mid]<=target){
+                rpos=mid;
+                l=mid+1;
+            } 
             else r=mid-1;
         }
-        return l;
-    }
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int lpos = lower_bound(nums,target); 
-        int rpos = lower_bound(nums,target+1)-1;
-        vector<int> res;
-        res.push_back(0<=lpos && lpos<nums.size() && nums[lpos]==target?lpos:-1);
-        res.push_back(0<=rpos && rpos<nums.size() && nums[rpos]==target?rpos:-1);
-        return res;
+
+        return {lpos,rpos};
     }
 };
