69a5f25364862c3c64790bf327579936a4e4eeb1
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-10 13:13:45 -0500

    Time: 0 ms (100%), Space: 15.1 MB (65.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    int binsearch(vector<int>& nums,int l,int r,int target){
+    int search(vector<int>& nums, int target) {
+        // if(nums.size()==1) return nums.back()==target?0:-1;
+        int l=0,r=nums.size()-1,ans=-1;
         while(l<=r){
             int mid = l+(r-l)/2;
+            if(nums[mid]>nums.back()){
+                ans=mid;
+                l=mid+1;
+            }
+            else r=mid-1;
+        }
+        // ans=nums[l]>nums.back()?l:ans;
+        if(ans==-1) {
+            l=0;r=nums.size()-1;
+        }
+        else{
+            if(target<=nums.back()) {
+                l=ans+1;
+                r=nums.size()-1;
+            }
+            else{
+                l=0;
+                r=ans;
+            }
+        }
+
+        while(l<=r){
+            int mid =l+(r-l)/2;
             if(nums[mid]==target) return mid;
             if(nums[mid]>target) r=mid-1;
             else l=mid+1;
         }
         return -1;
 
-    }
-    int search(vector<int>& nums, int target) {
-        int l=0,n=nums.size(),r=n-1;
-        int rot=-1;
-        if(nums.front()<nums.back() || nums.size()==1) return binsearch(nums,0,n-1,target);
-        while(l<r)
-        {
-            int mid = l+(r-l)/2; 
-            if(nums[mid]>nums[r]) {
-                rot=mid;
-                l=mid+1;
-            }
-            else r=mid; 
-        }
-        // if(rot==-1) return nums[l]>nums[l+1]
-        return nums.front()<=target && nums[rot]>=target?binsearch(nums,0,rot,target):
-                                                        binsearch(nums,rot+1,n-1,target);
     }
 };
