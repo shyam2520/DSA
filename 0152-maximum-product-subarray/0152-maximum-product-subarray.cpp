8c38e2576b1b3648cb824a794a4bdc0da34efc33
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-10 08:42:00 -0500

    Time: 0 ms (100%), Space: 17.8 MB (23.95%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-        priority_queue<int> pq;
+        int largest_neg=INT_MIN;
         int prod=1,res=nums[0],zc=0;;
         for(auto& i:nums){
             prod*=i;
             if(prod<0){
                 int temp = prod;
-                if(pq.size()) temp/=pq.top();
+                if(largest_neg) temp/=largest_neg;
                 res=max({res,temp,prod});
-                pq.push(prod);
+                // pq.push(prod);
+                largest_neg=max(largest_neg,prod);
             }
             else if(!prod){
                 res=max(res,prod);
                 prod=1;
-                pq=priority_queue<int>();
+                largest_neg=INT_MIN;
             }
             else res=max(res,prod);
         }
