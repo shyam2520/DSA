34540d02366d600b631db612d5f7a9e226574e36
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-10 19:54:01 -0500

    Time: 0 ms (100%), Space: 15.2 MB (65.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // if(nums.size()==1) return nums.back()==target?0:-1;
-        int l=0,r=nums.size()-1,ans=-1;
+        int l=0,r=nums.size()-1; 
         while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid]>nums.back()){
-                ans=mid;
-                l=mid+1;
-            }
-            else r=mid-1;
-        }
-        // ans=nums[l]>nums.back()?l:ans;
-        if(ans==-1) {
-            l=0;r=nums.size()-1;
-        }
-        else{
-            if(target<=nums.back()) {
-                l=ans+1;
-                r=nums.size()-1;
+            int mid = l+(r-l)/2; 
+            if(nums[mid]==target) return mid; 
+            if(nums[l]<=nums[mid]){
+                if(target>=nums[l] && nums[mid]>=target) r=mid-1; 
+                else l=mid+1;
             }
             else{
-                l=0;
-                r=ans;
+                if(target>=nums[mid] && target<=nums[r]) l=mid+1;
+                else r=mid-1;    
             }
         }
-
-        while(l<=r){
-            int mid =l+(r-l)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[mid]>target) r=mid-1;
-            else l=mid+1;
-        }
         return -1;
-
     }
 };
