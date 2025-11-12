7b9f72f21e906ba3a54ec471ea346e204a80b6a8
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-12 13:07:15 -0500

    Time: 0 ms (100%), Space: 13.2 MB (59.09%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        for(int i=0;i<arr.size();i++){
-            int missing_no=i==0?arr[i]-1:arr[i]-arr[i-1]-1;
-            if(k-missing_no<=0){
-                int l=i-1>=0?arr[i-1]+1:1,r=arr[i]-1,ans=-1;
-                int orgl=l,orgr=r;
-
-                while(l<=r){
-                    int mid=l+(r-l)/2;
-                    if((mid-orgl+1)<=k) {
-                        ans=mid;
-                        l=mid+1;
-                    }
-                    else r=mid-1;
-                }
-                return ans;
-            }
-            else k-=missing_no;
+        int l=0,r=arr.size()-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int missing=arr[mid]-(mid+1); 
+            if(missing>=k) r=mid-1;
+            else l=mid+1;
         }
-        if(k) return arr.back()+k;
-        return -1;
+        // r would be point the index which close to k elements ( opposite polarity)
+        return k+r+1;
+
     }
 };
