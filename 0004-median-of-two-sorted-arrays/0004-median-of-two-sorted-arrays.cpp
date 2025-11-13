ecb18cb00bc4d7e8994f67e7fa4d9f1747997371
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-13 18:22:01 -0500

    Time: 0 ms (100%), Space: 95.1 MB (79.95%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)   
-    { 
-        int m=nums1.size(),n=nums2.size();
-        int mid=(m+n)/2;
-        int k=0,i=0,j=0;
-        int prev=-1,curr=-1;
-        while(i<m && j<n && k<=(mid)){
-            if(nums1[i]<=nums2[j]){
-                prev=curr;
-                curr=nums1[i++];
-            } 
-            else {
-                prev=curr;
-                curr=nums2[j++];
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)  
+    {
+        int n1=nums1.size(),n2=nums2.size();
+        if(n1>n2){
+            return findMedianSortedArrays(nums2,nums1);
+            // we want to do binary search on smaller array more optimal
+        } 
+        int l=0,r=n1;
+        int left_half = (n1+n2+1)/2; // how many elements to be present in each half 
+        while(l<=r){
+            int mid1 = l+(r-l)/2;// no. of elements to consider for arr1 in left half
+            int mid2 = left_half - mid1;
+            int l1=INT_MIN,l2=INT_MIN;
+            int r1=INT_MAX,r2=INT_MAX;
+            if((mid1-1)>=0) l1=nums1[mid1-1];
+            if((mid2-1)>=0) l2=nums2[mid2-1];
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2) return max(l1,l2);
+                else return ((double)(max(l1,l2))+(double)(min(r1,r2)))/2.0;
             }
-            k++;
+            else if(l1>r2) r=mid1-1;
+            else l=mid1+1;
         }
-        while(i<m && k<=(mid)){
-            prev=curr;
-            curr=nums1[i++];
-            k++;
-        } 
-        while(j<n && k<=(mid)){
-            prev=curr;
-            curr=nums2[j++];
-            k++;
-        } 
 
-    
-        if((m+n)%2==0) return ((double)prev+(double)curr)/2.0;
-        return curr; 
+        return 0;
+
+
     }
 };
