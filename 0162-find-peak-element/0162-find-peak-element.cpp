fd74a0fb116bb9454ed97c5c3292ce67fb8744f8
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-17 08:18:17 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.size()==1) return 0;
-
-        int l=0,r=nums.size()-1; 
-        if(nums[l]>nums[l+1]) return l;
-        if(nums[r]>nums[r-1]) return r;
-        l++,r--;
+        int n=nums.size();
+        int l=0,r=n-1;
         while(l<=r){
             int mid = l+(r-l)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid; 
+            // cout<<mid<<endl;
+            if((mid==0 || nums[mid]>nums[mid-1]) && (mid==n-1 || nums[mid]>nums[mid+1])) return mid;
             if(nums[mid]>nums[mid-1]) l=mid+1;
             else r=mid-1;
         }
-        return 0;
+        return l;
     }
 };
