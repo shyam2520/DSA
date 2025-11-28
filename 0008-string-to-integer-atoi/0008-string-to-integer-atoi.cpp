0008502b579fd09754fa4155b8b4324e46426638
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-28 07:25:33 -0500

    Time: 0 ms (100%), Space: 9.2 MB (21.74%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i=0,n=s.length();
+        int i=0;
+        int n=s.length();
+        bool sign=false,neg=false;
         long long res=0;
-        bool neg=false,digit=false,sign=false;
-        while(i<n && isspace(s[i])) i++;
-        for(;i<n;i++){
-            if(isspace(s[i])) break;
-            if(s[i]=='+' || s[i]=='-') {
-                if(sign || digit || res!=0) break;
-                neg=s[i]=='-';
-                sign=true;
+        while(i<n && s[i]==' ') i++;
+        if(s[i]=='-' || s[i]=='+'){
+            sign=true;
+            neg=s[i]=='-'?true:false;
+            i++;
+        }
+        for(;i<n && res<INT_MAX;i++){
+            if(s[i]=='-' || s[i]=='+'){
+                break; 
             }
-            else if(isdigit(s[i])){
-                res=res*10+(s[i]-'0');
-                res = neg?-abs(res)<INT_MIN?INT_MIN:res:res>INT_MAX?INT_MAX:res;
-                digit=true;
-            } 
+            else if(isdigit(s[i])) res=res*10+(s[i]-'0');
             else break;
         }
+
         return neg?-res<INT_MIN?INT_MIN:-res:res>INT_MAX?INT_MAX:res;
     }
 };
