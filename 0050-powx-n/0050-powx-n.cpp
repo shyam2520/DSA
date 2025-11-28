35d4b9e65745d6f2775e9e0f255c1279ce7be353
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-28 10:31:26 -0500

    Time: 0 ms (100%), Space: 11 MB (7.18%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public: 
+    unordered_map<double,double> dp;
+    double calPower(double x,int pow){
+        // cout<<x<<" "<<pow<<endl;
+        if(dp.count(pow)) return dp[pow];
+        if(pow==0) return 1.0;
+        if(pow==1) return x;
+        if((pow%2)!=0) return dp[pow]=calPower(x,pow/2) * calPower(x,(pow/2)+1);
+        return dp[pow]=calPower(x,pow/2)*calPower(x,pow/2);
+    }
+
+    double myPow(double x, int n) {
+        double res;    
+        if(n==INT_MIN){
+            res=calPower(x,1)*calPower(x,INT_MAX);
+        }
+        else res=calPower(x,abs(n));
+        return n>0?res:1/res;
+    }
+};
