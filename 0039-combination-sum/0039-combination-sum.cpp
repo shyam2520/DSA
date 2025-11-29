d36dcfc3ca08094633981fb1df01c53d18f63ad4
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-29 15:47:05 -0500

    Time: 53 ms (11.62%), Space: 53.3 MB (9.66%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -7,17 +7,19 @@ public:
             res.push_back(temp);
             return ;
         }
-        if(idx==cand.size() || cand[idx]>k) return;
-        
+        if(idx==cand.size()) return;
+        if(cand[idx]>k){
+            combs(cand,k,idx+1,temp);
+            return;
+        }
         temp.push_back(cand[idx]);
         combs(cand,k-cand[idx],idx,temp);
         temp.pop_back();
         combs(cand,k,idx+1,temp);
-
     }
 
     vector<vector<int>> combinationSum(vector<int>& cand, int k) {
-        sort(begin(cand),end(cand));
+        // sort(begin(cand),end(cand));
         combs(cand,k,0,{});
         return res;
     }
