35cd6eb8485a079e5a67fffc530f011ea3c89c54
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-11-30 12:04:07 -0500

    Time: 83 ms (5.11%), Space: 40 MB (5.86%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+    void genqueen(int n,int rowidx,vector<string> board,unordered_set<int> row,unordered_set<int> col,
+    unordered_set<int> ldiag,unordered_set<int> rdiag){
+        if(!n) {
+            res.push_back(board);
+            return;
+        }
+        if(rowidx==board.size()) return;
+        
+        for(int i=0;i<board[0].size();i++){
+            int x=rowidx;
+            int y=i;
+            if(col.contains(y) || row.contains(x) || ldiag.contains(x+y) || rdiag.contains(y-x)) continue;
+            else{
+                board[x][y]='Q';
+                col.insert(y);row.insert(x);ldiag.insert(x+y);rdiag.insert(y-x);
+                genqueen(n-1,rowidx+1,board,row,col,ldiag,rdiag);
+                col.erase(y);row.erase(x);ldiag.erase(x+y);rdiag.erase(y-x);
+                board[x][y]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> temp(n,string(n,'.'));
+        //ldiag = i+j , rdiag= abs(i-j)
+        // cout<<temp.size()<<" "<<temp[0].size()<<endl;
+        unordered_set<int> row,col,ldiag,rdiag;
+        genqueen(n,0,temp,row,col,ldiag,rdiag);
+        return res;
+    }
+};
