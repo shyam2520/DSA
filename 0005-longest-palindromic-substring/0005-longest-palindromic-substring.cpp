183e3712f79302dd6f99fc883782f283fe2d5420
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-06 15:55:17 -0500

    Time: 7 ms (86.03%), Space: 14.8 MB (47.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-   pair<int,int> expandFromMiddle(string& s,int l,int r){
-        while(l>=0 && r<s.length() && s[l]==s[r]) {
+    int res = 1;
+    string ans="";
+    string longestPalindrome(string s) {
+        this->ans=s.back();
+        for(int i=0;i<s.length();i++){
+            isPalindrome(s,i,i);
+            isPalindrome(s,i,i+1);
+        }
+
+        return this->ans;
+    }
+
+    void isPalindrome(string& s,int l,int r){
+        while(l>=0 && r<s.length() && s[l]==s[r]){
             l--;
             r++;
         }
-        return {l+1,r-1};
-    }
-    string longestPalindrome(string s) {
-        string res = "";
-        for(int i=0;i<s.length();i++){
-            auto [l1,r1] = expandFromMiddle(s,i,i);
-            auto [l2,r2] = expandFromMiddle(s,i,i+1);
-            int len1 = r1-l1+1;
-            int len2 = r2-l2+1;
-            if(len1>res.length()) res=s.substr(l1,len1);
-            if(len2>res.length()) res=s.substr(l2,len2);
+        if(r-l>res){
+            this->ans=s.substr(l+1,r-l-1);
+            res=r-l;
         }
-        return res;
+        // return ;
     }
 };
-
