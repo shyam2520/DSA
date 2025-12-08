62abdd61f643a6508b4a28403f76b87c5b877760
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-08 11:22:25 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -4,9 +4,11 @@ public:
         long long res =0;
         for(auto& i:nums) res^=i;
         long long rsb = (res&res-1) ^ res;
-        // int rsb=res;
+        // long rsb=res;
         // while(rsb&1==0) rsb=rsb>>1;
-        // rsb = 1<<rsb;
+        // cout<<rsb<<endl;
+        // return {};
+        rsb = 1<<rsb;
         int b1=0,b2=0;
         for(auto& i:nums){
             if(i&rsb) b1^=i;
