6a94503f0b7cffe822583751b3624c795eff151c
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-09 17:28:10 -0500

    Time: 215 ms (24.59%), Space: 12.6 MB (90.75%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> dict(n+1,false);
+        vector<bool> dict(n,false);
         int cnt=0;
         for(int i=2;i<n;i++){
             if(dict[i]) continue;
             cnt++;
-            for(int j=i;(long)i*(long)j<=n;j++) dict[i*j]=true;
+            for(int j=i;(long)i*(long)j<n;j++) dict[i*j]=true;
         }
         return cnt;
     }
