938e6e66af2f640c37cbb7cd5cde1405df19505c
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-10 09:34:56 -0500

    Time: 1862 ms (2.12%), Space: 8.8 MB (13.94%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,17 +4,30 @@ public:
         bool powneg = n<0;
         bool neg_val = x<0;
         if(abs(x)==1) return n%2==0?1:neg_val?-1:1;
-        double res = powerVal(abs(x),abs(n));
         if(neg_val) neg_val=abs(n)&1;
+        double res=1; 
+        x=abs(x);
+        n=abs(n);
+        while(n>0 && res<=100000){
+            if(n&1==0)
+            {
+                x=(x*x);
+                n>>=1;
+            }
+            else{
+                res*=x;
+                n=n-1;
+            }
+        }
         res=powneg?1/res:res;
         return neg_val?-res:res;
     }
 
-    double powerVal(double x,long n){
-        if(n==1) return x;
-        if(n==0) return 1;
-        if(n%2) return x*powerVal(x,(n-1));
-        double ans = powerVal(x,n>>1);
-        return ans*ans;
-    }
+    // double powerVal(double x,long n){
+    //     if(n==1) return x;
+    //     if(n==0) return 1;
+    //     if(n%2) return x*powerVal(x,(n-1));
+    //     double ans = powerVal(x,n>>1);
+    //     return ans*ans;
+    // }
 };
