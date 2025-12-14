a328778558f9305acb19f44611b37b617452d1a3
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-14 09:14:41 -0500

    Time: 0 ms (100%), Space: 27.3 MB (29.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-    int trap(vector<int>& heights) {
-        // stack to push heights
-        // pop only when curr height >= stack.top()
-        // update inbetween to ensure  you subtract heights
-        stack<int> stack;
-        int res = 0;
-        for (int i = 0; i < heights.size(); i++) {
-            if(!heights[i]) continue;
-            if(stack.empty()) stack.push(i);
-            else{
-                int ih=0;
-                while(stack.size() && heights[stack.top()]<=heights[i])
-                {
-                    int idx = stack.top();
-                    int h = min(heights[stack.top()],heights[i]);
-                    int area = max(0,(h-ih)*(i-idx-1));
-                    res+=area;
-                    stack.pop();
-                    ih=max(ih,h);
-                }
-                if(stack.size()){
-                    int idx = stack.top();
-                    int h = min(heights[stack.top()],heights[i]);
-                    int area = max(0,(h-ih)*(i-idx-1));
-                    res+=area;
-                    ih=max(ih,h);
-                }
-                stack.push(i);
-            }
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> prefixMax(n),suffixMax(n);
+        prefixMax[0]=height[0];
+        suffixMax[n-1]=height[n-1];
+        int res=0;
+        for(int i=1;i<n;i++){
+            prefixMax[i]=max(height[i],prefixMax[i-1]);
+            suffixMax[n-i-1]=max(height[n-i-1],suffixMax[n-i]);
         }
-
+        for(int i=0;i<n;i++){
+            res+=max(0,min(prefixMax[i],suffixMax[i])-height[i]);
+        }
         return res;
     }
 };
