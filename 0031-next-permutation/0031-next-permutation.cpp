fba0c4d94d30785415d57845f4befd2c0602663e
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-19 18:43:32 -0500

    Time: 0 ms (100%), Space: 15.5 MB (91.7%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int max=0;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]>=max) max=nums[i];
-            else{
-                int rev=nums.size()-1;
-                while(rev>i && nums[rev]<=nums[i]) rev--;
-                swap(nums[rev],nums[i]);
-                sort(nums.begin()+i+1,nums.end());
-                return ;
+        // find the idx , idx<idx+1 
+        int r=nums.size()-2;
+        while(r>=0 && nums[r]>=nums[r+1]) {
+            r--;
+        }
+
+        if(r==-1) {
+            sort(begin(nums),end(nums));
+            return;
+        }
+
+        for(int i=nums.size()-1;i>r;i--){
+            if(nums[i]>nums[r]){
+                swap(nums[i],nums[r]);
+                break;
             }
         }
-        sort(begin(nums),end(nums));
-        return ;
+        
+        sort(begin(nums)+r+1,end(nums));
+
+
+        // 2,3,5,4,1 ->
+        // 1,2,3,4,5
+        // 1,2,4,3,5
+
+        // auto it = upper_bound(begin(nums),end(nums),nums[r]);
+        // swap idx with its next greater value 
+        
+        // sort everything from idx+1
     }
 };
