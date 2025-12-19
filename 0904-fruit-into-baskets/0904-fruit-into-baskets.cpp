62e8587ff356c61060ca8001d8266f1e5480c9cf
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-19 12:09:12 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+    int getTypes(vector<int>& umap){
+        int cnt=0;
+        for(auto& i:umap) {
+            if(i) cnt++;
+        }
+        return cnt;
+    }
     int totalFruit(vector<int>& fruits) {
         int anc=0;
         int n=fruits.size();
-        unordered_map<int,int> umap;
+        vector<int> umap(n,0);
         int res =0;
         for(int i=0;i<n;i++){
             umap[fruits[i]]++;
-            while(anc<=i && umap.size()>2){
+            while(anc<=i && getTypes(umap)>2){
                 umap[fruits[anc]]--;
-                if(umap[fruits[anc]]==0) umap.erase(fruits[anc]);
                 anc++;
             }
             res=max(res,i-anc+1);
