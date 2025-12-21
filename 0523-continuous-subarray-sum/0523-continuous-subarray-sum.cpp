dc36fc0b50b636d22e44e25bd67bf3a687f1d7da
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-21 15:14:14 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum =0,cntz=0;
+        unordered_map<int,int> dict;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(!nums[i]){
+                cntz++;
+                if(cntz>1) return true;
+            }
+            else cntz=0;
+            int rem = nums[i]%2;
+            if((sum%k==0 && i>=1) ) return true;
+            if(dict.count(sum%k)){
+                if(i-dict[sum%k]>=2) return true;
+            } 
+            else dict[rem]=i;
+        }
+        return false;
+    }
+};
