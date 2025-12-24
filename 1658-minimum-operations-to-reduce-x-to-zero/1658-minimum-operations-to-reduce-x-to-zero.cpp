1d33cc3a1c8219bc914856ce34c2f94820fac610
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-24 10:27:06 -0500

    Time: 4 ms (65.01%), Space: 102.4 MB (41.91%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -3,17 +3,18 @@ public:
     int minOperations(vector<int>& nums, int x) {
         int total=0;
         for(auto& i:nums) total+=i;
-        total-=x;
-        if(!total) return nums.size();
-        int i=0;
-        int sum=0,res=0;
-        for(int j=0;j<nums.size();j++)
-        {
-            sum+=nums[j];
-            while(i<=j && sum>total) 
-                sum-=nums[i++];
-            if(sum==total) res=max(res,j-i+1); 
+        int find = total-x;
+        if(find<0) return -1;
+        int sum=0,anc=0,res=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            while(anc<=i && sum>find) {
+                sum-=nums[anc++];
+            }
+            if(sum==find){
+                res=max(res,i-anc+1);
+            }
         }
-        return res==0?-1:nums.size()-res;
+        return res==INT_MIN?-1:nums.size()-res;
     }
 };
