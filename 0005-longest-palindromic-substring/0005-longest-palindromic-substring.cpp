73481dcd41fb51f928ae093b622a425225edcb0f
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-27 10:00:27 -0500

    Time: 7 ms (86.21%), Space: 9.4 MB (78.5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int res = 1;
-    string ans="";
-    string longestPalindrome(string s) {
-        this->ans=s.back();
-        for(int i=0;i<s.length();i++){
-            isPalindrome(s,i,i);
-            isPalindrome(s,i,i+1);
-        }
-
-        return this->ans;
-    }
-
-    void isPalindrome(string& s,int l,int r){
+    int res=0;
+    int st=-1,end=-1;
+    void checkPalindrome(string& s,int l,int r){
         while(l>=0 && r<s.length() && s[l]==s[r]){
             l--;
             r++;
         }
-        if(r-l>res){
-            this->ans=s.substr(l+1,r-l-1);
-            res=r-l;
+        
+        if((r-1)-(l+1)+1>res){
+            res=(r-1)-(l+1)+1;
+            end=r-1;
+            st=l+1;
         }
-        // return ;
+    }
+
+    string longestPalindrome(string s) {
+        
+        for(int i=0;i<s.length();i++){
+            checkPalindrome(s,i,i);
+            checkPalindrome(s,i,i+1);
+        }
+
+        return s.substr(st,res);
     }
 };
