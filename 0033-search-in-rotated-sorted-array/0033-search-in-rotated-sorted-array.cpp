d3ba4787e7e11cdeee587b8881086ac446c2f10a
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-26 23:03:49 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1; 
-        while(l<=r){
-            int mid = l+(r-l)/2; 
-            if(nums[mid]==target) return mid; 
-            if(nums[l]<=nums[mid]){
-                if(target>=nums[l] && nums[mid]>=target) r=mid-1; 
-                else l=mid+1;
-            }
-            else{
-                if(target>=nums[mid] && target<=nums[r]) l=mid+1;
-                else r=mid-1;    
-            }
+        if(nums.size()==1) return nums[0]==target?0:-1;
+        int mid, l = 0, r = nums.size() - 1;
+        while (l < r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r])
+                l = mid + 1;
+            else
+                r = mid;
         }
-        return -1;
+        int pivot = l,n=nums.size();
+        l = 0, r = nums.size() - 1;
+        // cout<<pivot<<endl;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            int realidx = (pivot+mid)%n;
+            // cout<<realidx<<endl;
+            if(nums[realidx]==target) return realidx;
+            if(nums[realidx]>target) r=mid-1;
+            else l=mid+1;
+        }
+        return nums[l]==target?l:-1;
     }
 };
