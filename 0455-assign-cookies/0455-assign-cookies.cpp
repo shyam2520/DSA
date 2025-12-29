783aee27509f4949367fa818d13f7eafab4188de
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-29 08:33:43 -0500

    Time: 13 ms (24.93%), Space: 47 MB (9.93%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,13 +3,14 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int m = g.size(), n = s.size();
         int cnt =0;
-        unordered_map<int,int> dict; 
+        map<int,int> dict; 
         for(int i=0;i<n;i++) dict[s[i]]++;
         for(int i=0;i<m;i++) {
-            if(dict.count(g[i]) && dict[g[i]]){
-                dict[g[i]]--;
-                cnt++;
-            }
+            auto it = dict.lower_bound(g[i]);
+            if(it==dict.end()) continue;
+            dict[it->first]--;
+            cnt++;
+            if(dict[it->first]<1) dict.erase(it->first);
         }
 
         return cnt;
