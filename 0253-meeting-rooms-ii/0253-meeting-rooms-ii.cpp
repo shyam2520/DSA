4b43b50a8c70e2cae01862afd11707403480530b
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-31 10:14:30 -0500

    Time: 0 ms (100%), Space: 16.4 MB (51.41%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,30 +1,39 @@
+struct meetings{
+    int start;
+    int end;
+};
+
+class compare{
+    public:
+    bool operator()(const meetings& a,const meetings& b){
+        if(a.start==b.start) return a.end<b.end;
+        return a.start<b.start;
+    }
+};
+
 class Solution {
 public:
+    using pii = pair<int,int>;
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        // [0,30 ],[3,8],[5,10],[15,20] 
-        // [0,30] ,[15,20]
-        // nlogn + nlogn
-        // O(n)
-        sort(begin(intervals),end(intervals), [](const vector<int>& a,const vector<int>& b){
-            if(a[0]==b[0]) {
-                return a[1]>b[1];
-            }
-            return a[0]<b[0];
-        });
-        priority_queue<int,vector<int>,greater<int>> pq;
-        int n = intervals.size() ;
-        // insert interval
-        // if start >endtime of top pop the stack 
-        // keep track max stack size 
-        int res =0;
+        int n = intervals.size();
+        vector<meetings> times;
+        meetings temp;
+        for(auto& i:intervals){
+            temp.start = i[0];
+            temp.end = i[1];
+            times.push_back(temp);
+        }
+        sort(times.begin(),times.end(),compare());
+        priority_queue<int,vector<int>,greater<>> pq;
+        int rooms =1;
         for(int i=0;i<n;i++){
-            while(pq.size() && pq.top()<=intervals[i][0]){
-                pq.pop();
-            }
-            pq.push(intervals[i][1]);
-            res=max(res,(int)(pq.size()));
+            // if(pq.empty()) pq.push()
+            while(pq.size() && times[i].start>=pq.top()) pq.pop();
+            pq.push({times[i].end});
+            rooms = max(rooms,(int)pq.size());
         }
 
-        return res;
+        return rooms;
+
     }
 };
