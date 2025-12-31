75119367193f07e46d0523754b3a7a8670abf467
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-31 12:04:40 -0500

    Time: 607 ms (7.82%), Space: 62.1 MB (6.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,7 +3,7 @@ public:
     // int dp[10001]={-1}
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
+        vector<int> dp(10001,-1);
         return canJump(0,nums,dp);
     }
 
