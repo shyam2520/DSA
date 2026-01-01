89a69d336b15b21046178bbf2d1003eaf2b7441d
Shyam Sundar Mylai <shyam2562000@gmail.com>
2025-12-31 19:58:10 -0500

    Time: 171 ms (11.46%), Space: 21.4 MB (25.71%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[10001] = {-1};
+    int jump(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return combs(0,nums);
+    }
+
+    int combs(int idx,vector<int>& nums){
+        if(idx>=nums.size()-1){
+            return 0;
+        }
+        if(dp[idx]!=-1) {
+            return dp[idx];
+        }
+
+        int steps = 1e9;
+        for(int i=1;i<=nums[idx];i++){
+            steps= min(steps,combs(idx+i,nums));
+        }
+        dp[idx]=1+steps;
+        return dp[idx];
+    }
+};
