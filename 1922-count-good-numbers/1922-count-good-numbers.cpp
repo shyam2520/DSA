e800b44ead2e003f42d26356d7a0ede2d5eb429e
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-02 19:45:43 -0500

    Time: 0 ms (100%), Space: 7.8 MB (69.13%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
-    long long mod=1e9+7;
-    long long powMod(int val, long long pow){
-        if(pow==0) return 1;
-        if(pow%2) return (val * powMod(val,pow-1)%mod)%mod;
-        long long res = (powMod(val,pow/2))%mod;
-        return (res * res)%mod;
+
+    long long mod = 1e9+7;
+    int countGoodNumbers(long long n) {
+        // n=3 
+        // 022
+        // 1-9 -> 5(e), 4(p)
+        // _,_,_
+        // power(5,100) -> res = (a*b)%mod -> (a%mod * b%mod)%mod
+        // 1. check for even len or odd len -> if even= n/2 , odd n/2+1 , 4 ->n/2 
+        // 2. pow(x,a)-> x^n = x^(n/2) * x^(n/2)
+        long long pow_ev=0,pow_od=0;
+        if(n%2){
+            pow_ev = n/2  + 1;
+            pow_od = n/2;
+        }
+        else{
+            pow_ev = n/2;
+            pow_od = n/2;
+        }
+        long long even = get_power(5,pow_ev)%mod;
+        long long odd = get_power(4,pow_od)%mod;
+        return (even*odd)%mod;
     }
 
-
-    int countGoodNumbers(long long n) {
-        long long evenIndices=n%2?n/2 + 1 :n/2;
-        long long oddIndices=n/2;
-        return (powMod(5,evenIndices) * powMod(4,oddIndices)) %mod;
+    long long get_power(long long x,long long pow){
+        if(pow==0) return (long long)1;
+        if(pow==1) return (long long)x;
+        if(pow%2){
+            long long ans = get_power(x,pow-1)%mod;
+            return ((x%mod) * ans)%mod;
+        }
+        else{
+            long long ans = get_power(x,pow/2)%mod;
+            return (ans*ans)%mod;
+        }
     }
 };
