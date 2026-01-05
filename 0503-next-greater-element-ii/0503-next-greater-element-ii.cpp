71340468949883e56184c104bc3894c4825ccfba
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-05 17:14:43 -0500

    Time: 7 ms (36.52%), Space: 28.4 MB (78.48%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int> stack;
-        int n = nums.size();
-        for(int i=0;i<n;i++) nums.push_back(nums[i]);
+        int n = nums.size(),curridx;
         vector<int> res(n,-1);
-        for(int i=0;i<nums.size();i++){
-            while(stack.size() && nums[stack.top()]<nums[i]){
-                if(stack.top()<n) res[stack.top()]=nums[i];
+        stack<int> stack;
+        for(int i=0;i<(2*n);i++){
+            // curridx=i%n;
+            while(stack.size() && nums[stack.top()%n]<nums[i%n]){
+                res[stack.top()%n]=nums[i%n];                
                 stack.pop();
             }
             stack.push(i);
         }
-
         return res;
     }
 };
