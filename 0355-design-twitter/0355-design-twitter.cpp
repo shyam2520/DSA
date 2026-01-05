4a504a8ef0e740bba6c632bba8210394a148f649
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-05 18:10:35 -0500

    Time: 10 ms (44.88%), Space: 55.7 MB (62.97%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,75 +1,61 @@
 class compare{
-    public: 
-    using Node = std::array<int,4>;
-    bool operator()(const Node& a,const Node& b){
+    public:
+    using td=std::array<int,4>;
+    bool operator()(const td& a,const td& b){
         return a[0]<b[0];
     }
 };
+
 class Twitter {
 public:
-    // 1. postTweet - > user <-> tweets : map(key,vector<(int,int)>)
-    // 2. follow/unfollow -> user <-> follower : map(key,unordered<int>)
-    // 3. user-> followers - O(1)
-    // 4.  for follow in followe - O(N)
-    // 5.       1
-    // 6.  pq= O(N) TC - Nlog(n) {time,tweet,idx,follwer}
-    // 7. 
-    using Node = std::array<int,4>;
     using pii = pair<int,int>;
-
-    unordered_map<int,vector<pii>> tweets;
-    unordered_map<int,unordered_set<int>> followers;
-    priority_queue<Node,vector<Node>,compare> pq;
-    int time = 0;
-
-    Twitter() {
-        
+    using td = std::array<int,4>; //{time,tweet,follower,idx}
+    // user->tweets([time,tweet]) 
+    // userid-> follwrIds
+    unordered_map<int,unordered_set<int>> fllwrs;
+    unordered_map<int,vector<pii>> twts;
+    int time=0;
+    Twitter() {        
     }
     
     void postTweet(int userId, int tweetId) {
         ++time;
-        tweets[userId].push_back({time,tweetId});
+        twts[userId].push_back({time,tweetId});
     }
     
     vector<int> getNewsFeed(int userId) {
-        // userId-> follow-> userId;
-        followers[userId].insert(userId);
-        for(auto& follower:followers[userId]){
-            // retrieve the last tweets 
-            int n = tweets[follower].size();
-            if(n==0) continue;
-            pii twt = tweets[follower][n-1];
-            pq.push({twt.first,twt.second,n-1,follower});
-        }
         vector<int> res;
-        int cnt =10;
-        while(pq.size() && cnt){
-            Node top_val = pq.top();
+        priority_queue<td,vector<td>,compare> pq;
+        // get the last tweet of each follower & push it to pq
+        fllwrs[userId].insert(userId);
+        for(auto fl:fllwrs[userId]){
+            int n = twts[fl].size();
+            if(n) pq.push({twts[fl][n-1].first,twts[fl][n-1].second,fl,n-1});
+        }
+        int cnt=10;
+        while(cnt && pq.size()){
+            td latest = pq.top();
             pq.pop();
-            res.push_back(top_val[1]);
-            // pq.push({})
-            int follower = top_val[3];
-            if(top_val[2]-1>=0)
+            res.push_back(latest[1]);
+            int follower = latest[2];
+            int idx = latest[3];
+            if(idx-1>=0)
             {
-                pii newtweet = tweets[follower][top_val[2]-1];
-                pq.push({newtweet.first,newtweet.second,top_val[2]-1,follower});
+                pii twt = twts[follower][idx-1];
+                pq.push({twt.first,twt.second,follower,idx-1});
             }
             cnt--;
         }
-
+        
         return res;
-
     }
     
     void follow(int followerId, int followeeId) {
-        followers[followerId].insert(followeeId);
+        fllwrs[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
-        if(followers[followerId].find(followeeId)==followers[followerId].end()){
-            return ;
-        }
-        followers[followerId].erase(followeeId);
+        fllwrs[followerId].erase(followeeId);
     }
 };
 
