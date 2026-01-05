e623ad920ad485b42bca1b35f3b80061c2bdc405
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-05 12:03:48 -0500

    Time: 47 ms (96.31%), Space: 172.8 MB (95.17%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,42 +1,69 @@
+class DLLNode{
+    public: 
+    int key,val;
+    DLLNode *next,*prev;
+    DLLNode(int key,int val){
+        this->key = key;
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
 class LRUCache {
 public:
-    int qsize=0;
     int cap;
-    queue<int> q;
-    unordered_map<int,int> dict,qdict;
-
-    LRUCache(int capacity) {
-        cap=capacity;
+    DLLNode *head,*tail;
+    unordered_map<int, DLLNode*> umap;
+    LRUCache(int capacity){
+        this->cap = capacity;
+        this->head = new DLLNode(-1,-1);
+        this->tail = new DLLNode(-1,-1);
+        this->head->next = tail;
+        this->tail->prev = head;
     }
     
+    void insertAfterHead(DLLNode* node){
+        DLLNode *nextNode = head->next;
+        node->prev = head;
+        node->next = nextNode;
+        head->next = node;
+        nextNode->prev = node;
+    }
+
+    void deleteNode(DLLNode* node){
+        DLLNode* prevNode = node->prev;
+        DLLNode* nextNode = node->next; 
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+        // delete(node);
+    }
+
     int get(int key) {
-        if(dict.count(key)){
-            qdict[key]++;
-            q.push(key);
-            return dict[key];
+        if(umap.find(key)==umap.end()) {
+            return -1;
         }
-        return -1;
+        else{
+            deleteNode(umap[key]);
+            insertAfterHead(umap[key]);
+            return umap[key]->val;
+        }
     }
     
     void put(int key, int value) {
-        if(!dict.count(key)){
-            dict[key]=value;
-            q.push(key);
-            qsize++;
-            qdict[key]++;
+        if(umap.find(key)==umap.end()){
+            DLLNode *node = new DLLNode(key,value);
+            insertAfterHead(node);
+            umap[key]=node;
         }
         else{
-            dict[key]=value;
-            q.push(key);
-            qdict[key]++;
+            deleteNode(umap[key]);
+            umap[key]->val = value;
+            insertAfterHead(umap[key]);
         }
-        while(qsize>cap){
-            qdict[q.front()]--;
-            if(!qdict[q.front()]){
-                dict.erase(q.front());
-                qsize--;
-            }
-            q.pop();
+        if(umap.size()>cap){
+            DLLNode *delNode = tail->prev;
+            deleteNode(delNode);
+            umap.erase(delNode->key);
         }
     }
 };
