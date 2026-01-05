965356f6bdbd488b6382590fc7e9de9e87538ea7
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-05 16:09:53 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // queue which keep track of the window
-        // if a new elment is greater than rest of the elements pop all front
-        // values in the queu;
-        vector<int> res;
-        deque<int> maxq;
-
-        for (int i = 0; i < nums.size(); i++) {
-            // insert at either front or back of queue
-            if (maxq.size() && nums[i] >= nums[maxq.front()]) {
-                while (maxq.size() && nums[i] >= nums[maxq.front()]) {
-                    maxq.pop_front();
-                }
-                maxq.push_front(i);
+        deque<int> dq; 
+        int n = nums.size();
+        vector<int> res; 
+        for(int i=0;i<n;i++){
+            // remove any values which out of window
+            while(dq.size() && dq.front()<=i-k){
+                dq.pop_front();
             }
-            else{
-                while(maxq.size() && nums[i]>=nums[maxq.back()]) maxq.pop_back();
-                maxq.push_back(i);
+            // if current value is greater that all the values in the window pop 
+            while(dq.size() && nums[dq.front()]<=nums[i]){
+                dq.pop_back();
             }
-            if (i+1 >= k) {
-                while(maxq.size() && maxq.front()<(i-k+1)) maxq.pop_front();
-                res.push_back(nums[maxq.front()]);
-            }
+            dq.push_back(i);
+            if(i+1>=k) res.push_back(nums[dq.front()]);
         }
 
-        // insert to queue
-        // pop if the max crosses window
-        // push to res
         return res;
     }
 };
