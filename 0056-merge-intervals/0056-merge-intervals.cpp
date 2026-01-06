65958ff2343114941e8ad3bfe87e34467a2f3891
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-06 09:59:10 -0500

    Time: 7 ms (41.98%), Space: 24.5 MB (21.47%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,18 +3,15 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(begin(intervals),end(intervals));
         vector<vector<int>> res;
-        for(auto& intr:intervals){
-            if(res.empty()) res.push_back(intr);
-            else{
-                int low=intr[0];
-                int high=intr[1];
-                while(res.size() && res.back()[1]>=low){
-                    low=min(low,res.back()[0]);
-                    high=max(high,res.back()[1]);
-                    res.pop_back();
-                }
-                res.push_back({low,high});
+        for(int i=0;i<intervals.size();i++){
+            int st = intervals[i][0];
+            int ed = intervals[i][1];
+            while(res.size() && res.back()[1]>=st){
+                st=min(st,res.back()[0]);
+                ed=max(ed,res.back()[1]);
+                res.pop_back();              
             }
+            res.push_back({st,ed});
         }
         return res;
     }
