b2417c82b24f6326b5acfb9dd2e5f7efd7dfa203
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-05 19:33:19 -0500

    Time: 0 ms (100%), Space: 8.8 MB (3.7%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        bool neg = false;
-        if (dividend > 0 && divisor < 0)
-        {    neg = true;}
-        if (dividend < 0 && divisor > 0)
-        {    neg = true;
-        }
-        // cout<<neg<<endl;
-        // if(divisor>dividend) return 0;
-
-        long div = abs((long)(dividend));
-        long d = abs((long)(divisor));
-
-        if (div == d)
-        {    return neg ? -1 : 1;}
-        long ans = 0;
-
-        while (div >= d) {
-            long cnt = 0;
-            while (div >= (d << (cnt + 1))) {
-                cnt++;
+    int divide(long long  dividend, long long  divisor) {
+        bool sign = false; 
+        if((dividend>0 && divisor<0) || (dividend<0 && divisor>0)) sign=true;
+        dividend = abs(dividend);
+        divisor = abs(divisor); 
+        long long  ans=0;
+        while(dividend>=divisor){
+            long long  n=0;
+            while(dividend>(pow(2,n+1)*divisor)){
+                n++;
             }
-            ans += ((long)1 << cnt);
-            div -= (d << (cnt));
+            dividend-=(pow(2,n)*divisor);            
+            ans+=pow(2,n);
+            if(ans>INT_MAX) break;
         }
-        cout<<ans<<endl;
-        if (ans >= INT_MAX && neg == false) {
-            return INT_MAX;
+        if(sign){
+            ans = -ans;
+            return ans<INT_MIN?INT_MIN:ans;
         }
-        if (ans >= INT_MAX && neg == true) {
-            return INT_MIN;
-        }
-
-        return neg ? -ans : ans;
+        return ans>INT_MAX?INT_MAX:ans;
     }
 };
