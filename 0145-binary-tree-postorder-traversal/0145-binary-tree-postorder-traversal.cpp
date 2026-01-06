a6f742b23db3944b5501fdc24fabc7d4b5340511
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-06 14:20:15 -0500

    Time: 0 ms (100%), Space: 11.2 MB (16.81%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,6 +11,13 @@
  */
 class Solution {
 public:
+    // 1. node traverse left->right->root
+    // 2. we have to push the final values into the stack at popping time 
+    // 3. catch is if we do it like pre/in order we end up printing root first 
+    // 4. so he we need to ensure that a root is print only after its lst & rst are parse
+    // 5. create pair - (node,'l/r') l -> lst is parsed, r -> rst is parsed 
+    // 6. only pop out when the top value of stack is 'r'  
+
     vector<int> res;
     using ptc= pair<TreeNode*,char>;
     vector<int> postorderTraversal(TreeNode* root) {
@@ -18,6 +25,7 @@ public:
         TreeNode* node = root; 
         while(node || stack.size()){
             if(node){
+                // 
                 stack.push({node,'l'});
                 node=node->left;
             }
