e26bb54017c8c6c17573f6ba67fab2ec397d1460
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-09 17:55:18 -0500

    Time: 7 ms (31.07%), Space: 37.5 MB (44.35%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // have two stacks (left , right)
+    // 1. left - insert in ascending
+    // 2. right - insert in descending
+    // 3.  do two sum based on comparing the values  like we did for
+    // vectors 
+    void insertAsc(TreeNode* node,stack<TreeNode*>& stk){
+        while(node){
+            stk.push(node);
+            node=node->left;
+        }
+    }
+
+    void insertDesc(TreeNode* node,stack<TreeNode*>& stk){
+        while(node){
+            stk.push(node);
+            node=node->right;
+        }
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+        stack<TreeNode*> left, right; 
+        insertAsc(root,left);
+        insertDesc(root,right);
+        while(left.size() && right.size()){
+            int l=left.top()->val;
+            int r=right.top()->val;
+            // if(l>root->val || r<=root->val) return false;
+            if(l+r==k && left.top()!=right.top()) return true;
+            if(l+r>k){
+                TreeNode* node = right.top();
+                right.pop();
+                insertDesc(node->left,right);
+            }
+            else {
+                TreeNode* node = left.top();
+                left.pop();
+                insertAsc(node->right,left);
+            }
+        }
+        return false;
+    }
+};
