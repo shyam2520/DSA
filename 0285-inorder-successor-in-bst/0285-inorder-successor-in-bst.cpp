8b3631758a59d8baeaa06e9b568f06eaafa1e503
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-09 16:34:52 -0500

    Time: 19 ms (82.86%), Space: 23.1 MB (16.11%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
@@ -10,33 +10,21 @@
 class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        if(!root) return NULL; 
-        return traverse(root,p,NULL);
-    }
-
-    // when you find the element possible next success
-    // if parent > child (lst) -> either parent or left most child of its rst 
-    // if parent < child ( rst) -> left most child 
-    // if root ==p the 
-
-    TreeNode* traverse(TreeNode* node, TreeNode* p, TreeNode* larger){
-        if(!node) return NULL; 
-        if(node==p){
-            // check if its lst or rst 
-            if(larger){
-                TreeNode* succ = inordersuccessor(node->right);
-                return succ?succ:larger;
+        TreeNode *node = root,*successor = NULL; 
+        while(node){
+            if(node==p){
+                TreeNode *temp = node->right; 
+                while(temp && temp->left) temp=temp->left;
+                return temp?temp:successor;
             }
-            else return inordersuccessor(node->right);
+            if(node->val>p->val){
+                successor=node;
+                node=node->left;
+            }
+            else {
+                node=node->right;
+            }
         }
-        if(node->val>p->val) return traverse(node->left,p,node);
-        else return traverse(node->right,p,larger);
-
-    }
-
-    TreeNode* inordersuccessor(TreeNode* node){
-        if(!node) return NULL; 
-        while(node->left) node=node->left;
-        return node;
+        return NULL; 
     }
 };
