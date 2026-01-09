51ef8e2e4776365a0205e39fd0f125da7efd7dfa
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-09 13:39:59 -0500

    Time: 0 ms (100%), Space: 21.9 MB (44.77%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,13 +11,17 @@
  */
 class Solution {
 public:
-    bool isValidBST(TreeNode* node,int min_val=INT_MIN,int max_val = INT_MAX) {
-        //1. at each node you set range it has to between those values
+    bool isValidBST(TreeNode* node,
+                    TreeNode* minNode=NULL,
+                    TreeNode* maxNode=NULL) 
+    {
+        //1. at each node you set range it has to between those     values
         //2. and as you move left or right you update those ranges 
         if(!node) return true;
-        if(!(node->val>min_val && node->val<max_val)) return false;
-        return isValidBST(node->left,min_val,node->val) && 
-            isValidBST(node->right,node->val,max_val);
-
+        // if(!(node->val>min_val && node->val<max_val)) return false;
+        if(minNode && node->val<=minNode->val) return false; 
+        if(maxNode && node->val>=maxNode->val) return false;
+        return isValidBST(node->left,minNode,node) && 
+            isValidBST(node->right,node,maxNode);
     }
 };
