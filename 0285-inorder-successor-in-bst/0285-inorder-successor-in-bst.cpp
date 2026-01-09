8a231b0c1c6f4149473d177ce634811548e81f1a
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-09 16:17:45 -0500

    Time: 26 ms (26.6%), Space: 22.9 MB (96.93%) - LeetHub

---
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if(!root) return NULL; 
+        return traverse(root,p,NULL);
+    }
+
+    // when you find the element possible next success
+    // if parent > child (lst) -> either parent or left most child of its rst 
+    // if parent < child ( rst) -> left most child 
+    // if root ==p the 
+
+    TreeNode* traverse(TreeNode* node, TreeNode* p, TreeNode* parent){
+        if(!node) return NULL; 
+        if(node==p){
+            // check if its lst or rst 
+            if(parent){
+                TreeNode* succ = inordersuccessor(node->right);
+                return succ?succ:parent;
+            }
+            else return inordersuccessor(node->right);
+        }
+        if(node->val>p->val) return traverse(node->left,p,node);
+        else return traverse(node->right,p,parent);
+
+    }
+
+    TreeNode* inordersuccessor(TreeNode* node){
+        if(!node) return NULL; 
+        while(node->left) node=node->left;
+        return node;
+    }
+};
