5520426947b8a375f6753f9c1ba1b458cb6faf34
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-09 19:23:14 -0500

    Time: 1 ms (8.45%), Space: 13.1 MB (90.13%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int l=0,r=arr.size()-1;
+        // [2,3,4,7,11] k= 2,
+        // [0,1,2,3,4]
+        // [1,2,3,6,10]
+        // [1,1,1,3,6]  
+        
+        // [1,2,3,4,5,6,7,8,9,10,11] 
+        // TC - O(logn)
+        int l = 0, r= arr.size()-1; 
+        int res = -1;
+        if((arr[l]-1)>=k) return k;
         while(l<=r){
-            int mid=l+(r-l)/2;
-            int missing=arr[mid]-(mid+1); 
-            if(missing>=k) r=mid-1;
-            else l=mid+1;
+            int mid = l+(r-l)/2; 
+            int val = arr[mid]; // 11 
+            int missing_elements = (val-1) - mid;// actualidx(val-1) - mid( curr_idx)
+            if(missing_elements>=k){
+                r=mid-1;
+            }
+            else{
+                res=mid; 
+                l=mid+1;
+            }
         }
-        // r would be point the index which close to k elements ( opposite polarity)
-        return k+r+1;
-
+        // cout<<res<<" "<<arr[res]<<endl;
+        // int res = r;
+        int missing_elements = (arr[res]-1) - res;
+        cout<<missing_elements<<endl;
+        // if(missing_elements<=k){
+        k-=missing_elements;
+        cout<<k<<endl;
+        return arr[res]+k;
+        // } 
+        // return arr[res]-k;
+        // missing>=k;
     }
 };
