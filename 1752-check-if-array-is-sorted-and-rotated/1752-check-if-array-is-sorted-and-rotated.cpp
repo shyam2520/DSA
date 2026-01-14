8fa0f1a93599299f6f5a9fa5162f3c8d575526b0
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-14 10:23:26 -0500

    Time: 0 ms (100%), Space: 11.2 MB (75.9%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]>=nums[i-1]) continue;
-            else{
-                int peak = nums[i-1]; 
-
-                while(i+1<nums.size() && nums[i]<=nums[i+1]) i++; 
-                if(i==nums.size()-1 && nums[i]<=peak && nums.front()>=nums[i]) return true;
-                else return false;
+        // find rotate pos
+        int n = nums.size(), i=0;  
+        for(i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                break;
             }
         }
-
+        if(i==n) return true; 
+        for(int j=i+1;(j%n)!=i;j++){
+            int curr = j%n; 
+            int prev = (j-1)%n;
+            if(!(nums[curr]>=nums[prev])) return false;
+        }
         return true;
     }
 };
