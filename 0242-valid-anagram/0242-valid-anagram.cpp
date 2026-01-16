04f931c83b73e7c77ee0818f4a45567fbc788883
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-16 14:14:18 -0500

    Time: 0 ms (100%), Space: 9.6 MB (91.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int m=s.length(),n=t.length();
-        if(m!=n) return false;
-        vector<int> dict(26);
-        for(int i=0;i<m;i++){
-            dict[s[i]-'a']++;
-            dict[t[i]-'a']--;
+        vector<int> dicts(26);
+        if(s.length()!=t.length()) return false;
+        for(int i=0;i<s.length();i++){
+            dicts[s[i]-'a']++;
+            dicts[t[i]-'a']--;
         }
 
-        for(auto& i:dict) if(i) return false;
+        for(auto& i:dicts) if(i!=0) return false; 
         return true;
     }
 };
