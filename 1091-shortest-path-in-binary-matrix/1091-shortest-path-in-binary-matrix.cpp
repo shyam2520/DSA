6df8f3562026421ac1f9f881c0809a2953058681
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-22 15:14:37 -0500

    Time: 16 ms (64.23%), Space: 23.7 MB (73.99%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
+    using pii = pair<int,int>; 
+    vector<vector<int>> dir = {
+        {-1,0},{1,0},{0,-1},{0,1},
+        {1,1},{1,-1},{-1,-1},{-1,1}
+    };
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        vector<pair<int,int>> dir={{0,1},{0,-1},{1,0},{-1,0},{-1,-1},{1,1},{1,-1},{-1,1}};
-        int m=grid.size(),n=grid[0].size();
-        if(grid[0][0]==1 || grid[m-1][n-1]==1) return -1;
-        if(m==1 && n==1 && grid[0][0]==0)return 1;
-        queue<pair<int,int>> q;
-        // cout<<"chec"<<endl;
+        // unordered_set<pii> visited; 
+        // visited.insert({0,0});
+        if(grid[0][0]==1) return -1;
+        queue<pii> q;
         q.push({0,0});
+        int cnt = 1;
+        int m=grid.size(),n=grid[0].size();
+        if(grid[0][0]==0 && m-1==0 && n-1==0) return 1;
         grid[0][0]=1;
-        int len=1;
-        int row,col;
-        // cout<<dir.size()<<endl;
-        // pair<int,int> temp;
-        while(q.size())
-        {
-            for(int i=q.size();i>0;i--)
-            {
-                // cout<<i<<endl;
-                row=q.front().first;
-                col=q.front().second;
-                q.pop();
-                for(int i=0;i<8;i++)
-                {
-                    int nrow=row+dir[i].first;
-                    int ncol=col+dir[i].second;
-                    // cout<<nrow<<" "<<ncol<<endl;
-                    if(nrow==m-1 && ncol==n-1) return len+1;
-                    if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && grid[nrow][ncol]!=1)
-                    {
-                         // cout<<nrow<<" "<<ncol<<endl;
-
-                        q.push({nrow,ncol});
-                        grid[nrow][ncol]++;
-                    }
+        while(q.size()){
+            int qs = q.size();
+            for(int i=0;i<qs;i++){
+                pii front = q.front(); 
+                q.pop(); 
+                for(auto& d:dir){
+                    int x = d[0]+front.first;
+                    int y = d[1]+front.second;
+                    if(x<0 || x==m || y<0 || y==n || grid[x][y]==1 ) continue;
+                    // cout<<x<<" "<<y<<" "<<grid[x][y]<<endl; 
+                    if(x==m-1 && y==n-1) return cnt+1;
+                    grid[x][y]=1; 
+                    q.push({x,y});
                 }
             }
-            len++;
+            cnt++;
         }
         return -1;
     }
