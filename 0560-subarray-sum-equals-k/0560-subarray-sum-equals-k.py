a73f224b9f85a69049c9797b1e7c40e89aee8e22
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-23 20:01:32 -0500

    Time: 22 ms (96.96%), Space: 21.9 MB (35.4%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,13 @@
-from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        n=len(nums)
-        hash_map = defaultdict(int)
-        hash_map[0]+=1
-        res=0
-        curr_sum = 0
-        for i in range(len(nums)): 
-            curr_sum+=nums[i]
-            if curr_sum-k in hash_map: 
-                res+=hash_map[curr_sum-k]
-            hash_map[curr_sum]+=1 
-        return res 
-        
+        key_cnt = {} 
+        curr_sum = 0 
+        key_cnt[0]=1
+        total = 0
+        for val in nums: 
+            curr_sum+=val 
+            if curr_sum-k in key_cnt: 
+                total+=key_cnt[curr_sum-k]
+            key_cnt[curr_sum]=key_cnt.get(curr_sum,0)+1
+
+        return total 
