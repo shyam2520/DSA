e159205e32b6e284f5fd675536f672d8f8c7d03e
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-25 15:05:40 -0500

    Time: 0 ms (100%), Space: 27.1 MB (25.65%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,23 +1,24 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        l,r=0,len(nums)-1 
-        while(l<=r): 
+        # get the mid 
+        # if the compare with left and right if its not equal thats the resul 
+        # if prev is same exclude that and get the cnt 
+        l,r = 0,len(nums)-1 
+        while l<=r: 
             mid=l+(r-l)//2 
-            left_val =  float("-inf") if mid-1<0 else nums[mid-1]
-            right_val = float("-inf") if mid+1>=len(nums) else nums[mid+1]
-            if nums[mid]!=left_val and nums[mid]!=right_val: 
+            prevele = nums[mid-1] if mid-1>=0 else float("-inf")
+            nextele = nums[mid+1] if mid+1<len(nums) else float("-inf")
+
+            if nums[mid]!=prevele and nums[mid]!=nextele: 
                 return nums[mid]
-            if nums[mid]==right_val:
-                size = r-(mid+1)
-                if size%2: 
-                    l=mid+2 
-                else:
-                    r=mid-1 
+            
+            leftcnt = mid-l
+            leftcnt = leftcnt-1 if nums[mid]==prevele else leftcnt
+            leftcnt = max(0,leftcnt)
+            
+            if leftcnt%2!=0:
+                r = mid-2 if nums[mid]==prevele else mid -1 
             else: 
-                size = mid-1 
-                if size%2: 
-                    r=mid-2 
-                else:
-                    l=mid+1
-        
-        return -1
+                l= mid+2 if nums[mid]==nextele else  mid+1 
+            
+        return -1 
