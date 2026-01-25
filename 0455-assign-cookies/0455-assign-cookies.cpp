e4730cfcfd9682e1e439e055b802b96efd586a5a
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-24 19:54:21 -0500

    Time: 8 ms (60.98%), Space: 44.9 MB (45.7%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int m = g.size(), n = s.size();
+    
+        sort(begin(g),end(g));
+        sort(begin(s),end(s));
+        int l1=0,l2=0;
         int cnt =0;
-        map<int,int> dict; 
-        for(int i=0;i<n;i++) dict[s[i]]++;
-        for(int i=0;i<m;i++) {
-            auto it = dict.lower_bound(g[i]);
-            if(it==dict.end()) continue;
-            dict[it->first]--;
-            cnt++;
-            if(dict[it->first]<1) dict.erase(it->first);
+        while(l1<g.size() && l2<s.size()){
+            while(l1<g.size() && l2<s.size() && g[l1]>s[l2]){
+                l2++;
+            }
+            if(l2!=s.size()){
+                cnt++;l1++;l2++;
+            }
         }
 
         return cnt;
