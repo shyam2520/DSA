18fdc3049d14f4fbeeb18080222265483de3fd3c
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-25 15:27:54 -0500

    Time: 0 ms (100%), Space: 19.6 MB (14.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,30 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l,r=0,len(nums)-1 
+
+    def binsearch(self,nums,l,r,target): 
         while l<=r: 
             mid = l+(r-l)//2 
             if nums[mid]==target: 
                 return mid 
-            if nums[l]<=nums[mid]:
-                if nums[l]<=target<=nums[mid]: 
-                    r=mid-1
-                else: 
-                    l=mid+1 
+            elif nums[mid]>target: 
+                r=mid-1 
             else: 
-                if nums[mid]<=nums[r] and nums[mid]<=target<=nums[r]: 
-                    l=mid+1
-                else: 
-                    r=mid-1
-                # l=mid+1 
-        return -1  
+                l=mid+1 
+        return -1 
 
+    def search(self, nums: List[int], target: int) -> int:
+        l,r = 0,len(nums)-1 
+
+        peak=-1 
+        while l<=r: 
+            mid = l+(r-l)//2 
+            if nums[mid]>nums[-1]: 
+                peak=mid
+                l=mid+1 
+            else: 
+                r=mid-1 
+
+        if peak==-1: 
+            return self.binsearch(nums,0,len(nums)-1,target) 
+        if nums[0]<=target<=nums[peak]:
+            return self.binsearch(nums,0,peak,target) 
+        return self.binsearch(nums,peak+1,len(nums)-1,target)
