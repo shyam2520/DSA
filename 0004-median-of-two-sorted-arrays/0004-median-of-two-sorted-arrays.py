75a963d50d2c576aca439b066af24848128f0ab6
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-29 13:25:35 -0500

    Time: 8 ms (9.08%), Space: 19.7 MB (18.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,35 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # m+n is even mean m+n/2 would be the barrier
-        # if odd (m+n+1)/2 would be the barries 
-        # so in that mean left of barrier of barrier elements in total 
+        # creater a barrier for m+n//2 elements 
+        # at each point compare the two elements with cross data 
+        # if true then that would be median if not move accoridngly 
+        # to optmize we do bin search on smaller value 
+        
         m,n = len(nums1),len(nums2) 
-        k = 0 
         if m>n: 
             return self.findMedianSortedArrays(nums2,nums1)
-        if (m+n)%2: 
-            k=(m+n+1)//2
-        else: 
-            k=(m+n)//2
-        
-        # for range l,r in m we would mid element and nums 2  k-mid 
-        # compare left and right for each value based on that we move the ptrs 
-        l,r=0,m 
+        k = (m+n+1)//2
+        l,r = 0,m 
         while l<=r: 
-            mid = l+(r-l)//2 
-            nums1l = nums1[mid-1] if mid-1>=0 else float("-inf")
-            nums2l = nums2[k-mid-1] if k-mid-1>=0  else float("-inf")
-            nums1r = nums1[mid] if mid<m else float("inf")
-            nums2r = nums2[k-mid] if k-mid<n else float("inf")
+            mid1 = l+(r-l)//2 # elements from m  
+            mid2 = k-mid1 # elements from n 
+            nums1l = nums1[mid1-1] if mid1>0 else float("-inf")
+            nums2l = nums2[mid2-1] if mid2>0 else float("-inf")
+            nums1r = nums1[mid1] if mid1<m else float("inf")
+            nums2r = nums2[mid2] if mid2<n else float("inf")
             if nums1l>nums2r: 
-                r=mid-1
+                r=mid1-1 
             elif nums2l>nums1r:
-                l=mid+1
+                l=mid1+1 
             else: 
                 if (m+n)%2: 
                     return max(nums1l,nums2l)
                 else: 
-                    a = max(nums1l,nums2l)
-                    b = min(nums2r,nums1r)
-                    return (a+b)/2 
+                    return (max(nums1l,nums2l)+min(nums1r,nums2r))/2
+
         return -1 
