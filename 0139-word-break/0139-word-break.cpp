13c8324bcebe57c34a2668178cb54a96e8ac6696
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-28 20:47:56 -0500

    Time: 28 ms (7.28%), Space: 21.9 MB (7.55%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    bool searchString(string& s,unordered_map<string,int>& dict,int l,int r ,string temp,int& max_length,vector<vector<int>>& dp){
-        if(temp.length()>max_length) return false;
-        if(r==s.length()) return !temp.size();
-        if(dp[l][r]!=-1) return dp[l][r];
-        temp.push_back(s[r]);
-        bool a = searchString(s,dict,l,r+1,temp,max_length,dp);
-        if(dict.count(temp)){
-            bool b= searchString(s,dict,r+1,r+1,"",max_length,dp);
-            dp[l][r]=a||b?1:0;
-            return dp[l][r];
-        }
-        dp[l][r]= a?1:0;
-        return dp[l][r];
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string,int>  dict;
-        int max_length=0;
-        int n=s.length();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        for(auto& i:wordDict){
-            max_length=max(max_length,(int)i.size());
-            dict[i]++;
-        } 
-        return searchString(s,dict,0,0,"",max_length,dp);
+        unordered_map<string,int> dict;
+        vector<int> dp(s.length(),-1);
+        for(auto& word:wordDict) dict[word]++;
+        return dfs(s,0,dict,dp);
+    }
+
+    bool dfs(string& s,int start,unordered_map<string,int>& dict,vector<int>& dp){
+        if(start==s.length()) return true;
+        if(dp[start]!=-1) return dp[start]?true:false;
+        bool res = false;
+        for(int i=start;i<s.length();i++){
+            string temp = s.substr(start,i-start+1);
+            if(dict[temp]){
+                res |= dfs(s,i+1,dict,dp);
+                // if(res) return true;
+            }
+        }
+
+        // return res;
+        dp[start]=res?1:0;
+        return res;
     }
 };
