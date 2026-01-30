d048843a3af417c915395b79d78d558f080e7130
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-01-30 18:52:41 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int a=-1,b=-1,c=-1,res=0;
-        for(int i =0;i<s.length();i++){
-            if(s[i]=='a') a=i;
-            if(s[i]=='b') b=i;
-            if(s[i]=='c') c=i;
-            if(a!=-1 && b!=-1 && c!=1) res+=min({a,b,c})+1;
+        // 1. find the smallest valid window - n - i
+        // 2. make window invalid if still valid keep adding extra 
+        int anc=0,res=0;
+        int a=0,b=0,c=0,n=s.size(); 
+        for(int i=0;i<s.size();i++){
+            char ch = s[i];
+            a+=ch=='a'?1:0; 
+            b+=ch=='b'?1:0; 
+            c+=ch=='c'?1:0;
+            if(a && b && c){
+                int subcnt = n-i; 
+                res+=subcnt;
+                char ancch = s[anc++];
+                if(ancch=='a') a--;
+                if(ancch=='b') b--; 
+                if(ancch=='c') c--;
+            }
+            while(a && b && c && anc<=i ){
+                res++;
+                char ancch = s[anc++];
+                if(ancch=='a') a--;
+                if(ancch=='b') b--; 
+                if(ancch=='c') c--;
+            }
         }
+
         return res;
+        
     }
 };
