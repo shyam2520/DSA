45d471e005b6ed2adf2527251cf5e72ef05b09a6
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-01 17:54:05 -0500

    Time: 3 ms (40.73%), Space: 15.6 MB (13.66%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    void combs(vector<int>& cand,int idx,int t,vector<int> temp){
-        if(!t) {
+    vector<vector<int>> res; 
+    void recursion(vector<int>& cand,int st,int target,vector<int> temp){
+        if(target==0){
             res.push_back(temp);
             return;
         }
-        if(idx==cand.size() || cand[idx]>t){
-            return;
+        if(st==cand.size()){
+            return ;
         }
-        temp.push_back(cand[idx]);
-        combs(cand,idx+1,t-cand[idx],temp);
-        temp.pop_back();
-        while(idx+1<cand.size() && cand[idx]==cand[idx+1]) idx++;
-        combs(cand,idx+1,t,temp);
+
+        for(int i=st;i<cand.size();i++){
+            if(i>st && cand[i]==cand[i-1]) continue; 
+            if(cand[i]>target) return;
+            temp.push_back(cand[i]); 
+            recursion(cand,i+1,target-cand[i],temp);
+            temp.pop_back();
+        }
     }
-    vector<vector<int>> combinationSum2(vector<int>& cand, int t) {
-        sort(begin(cand),end(cand));
-        combs(cand,0,t,{});
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // at each start point if i>start skip duplicates 
+        sort(begin(candidates),end(candidates)); 
+        recursion(candidates,0,target,{});
         return res;
+        
     }
 };
