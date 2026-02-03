fa155de32a43d08f64e383107cb17836ba3267e1
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-03 17:41:26 -0500

    Time: 55 ms (26.86%), Space: 66.3 MB (22.45%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adjMat, vector<int>& visited,
-             vector<int>& pathvisited, vector<bool>& nodes, int node) {
-
-        visited[node] = 1;
-        pathvisited[node] = 1;
-        for (auto& neigh : adjMat[node]) {
-            if (visited[neigh] != -1) {
-                if (pathvisited[neigh] == 1) {
-                    nodes[node]=false;
-                    return false;
-                }
-            } else {
-                bool ans = dfs(adjMat, visited, pathvisited, nodes, neigh);
-                nodes[node]=nodes[node]&ans;
-                if (!ans) return false;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        // reverse the direction of nodes in graph for adjMat 
+        // calculate in the indegree 
+        // khan's algo 
+        int n=graph.size();
+        vector<vector<int>> adjMat(n);
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++){
+            int u = i; // u->v to v->u
+            for(auto& v:graph[i]){
+                adjMat[v].push_back(u);
+                indegree[u]++;
             }
         }
-
-        pathvisited[node] = -1;
-        nodes[node]=nodes[node]&true;
-        return nodes[node];
-    }
-
-    vector<int> eventualSafeNodes(vector<vector<int>>& adjMat) {
-        int n = adjMat.size();
-        vector<bool> nodes(n, true);
-        vector<int> visited(n, -1), pathvisited(n, -1);
-
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == -1) {
-                bool ans = dfs(adjMat, visited, pathvisited, nodes, i);
-                nodes[i]= nodes[i]&ans;
+        vector<int> res; 
+        queue<int> q; 
+        for(int i=0;i<n;i++) if(!indegree[i]) q.push(i);
+        while(q.size()){
+            int front = q.front();
+            q.pop();
+            res.push_back(front);
+            for(auto& neigh:adjMat[front]){
+                indegree[neigh]--;
+                if(!indegree[neigh]) q.push(neigh);
             }
         }
-        vector<int> res;
-        for (int i = 0; i < n; i++) if (nodes[i]) res.push_back(i);
+        sort(begin(res),end(res));
         return res;
     }
 };
