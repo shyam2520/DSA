79447a5b0b52e1ee2b34998a527d1275631ca1a3
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-11 08:49:56 -0500

    Time: 0 ms (100%), Space: 10.3 MB (65.03%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -3,27 +3,34 @@ public:
     int rob(vector<int>& nums){
         int n = nums.size();
         if(n==1) return nums.back();
-        vector<int> dp(n,-1);
-        int next=0,next2=0;
-        int a = houserob(nums,0,n-2,dp);
+        // vector<int> dp(n,-1);
+        // int next=0,next2=0;
+        int a = houserob(nums,0,n-2);
         // dp=vector<int>(n,-1);
-        int b = houserob(nums,1,n-1,dp);
+        int b = houserob(nums,1,n-1);
         return max(a,b);
     }
 
-    int houserob(vector<int>& nums,int s,int e,vector<int> dp){
+    int houserob(vector<int>& nums,int s,int e){
         // if(idx>=e) return 0;
         // if(dp[idx]!=-1) return dp[idx];
         // int a = houserob(nums,idx+1,e,dp);
         // int b = nums[idx]+houserob(nums,idx+2,e,dp);
         // return dp[idx]=max(a,b);
-        dp[e]=nums[e];
-
+        
+        int next = nums[e];
+        int next2 = 0;
+        int curr = 0;
         for(int i=e-1;i>=s;i--){
-            int a = dp[i+1];
-            int b= nums[i]+(i+2>e?0:dp[i+2]);
-            dp[i]=max(a,b);
+            int a = next;
+            int b = nums[i]+next2;
+            curr = max(a,b);
+            next2 = next;
+            next = curr;
+            // int a = dp[i+1];
+            // int b= nums[i]+(i+2>e?0:dp[i+2]);
+            // dp[i]=max(a,b);
         }
-        return max(dp[s],dp[e]);
+        return next;
     }
 };
