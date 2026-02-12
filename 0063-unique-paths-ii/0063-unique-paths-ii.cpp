a03e0063f8839bf43b77784b8aecb656f9ddda3e
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-12 11:21:40 -0500

    Time: 0 ms (100%), Space: 11.4 MB (92.67%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -9,14 +9,16 @@ public:
         vector<int> curr(n);
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
-                if(grid[i][j]==1) curr[j]=0;
+                if(grid[i][j]==1) {
+                    curr[j]=0;
+                }
                 else if(i==m-1 && j==n-1) curr[j]=1;
                 else{
-                    curr[j]=((long)((i+1<m?next[j]:0)%mod)+(long)((j+1<n?curr[j+1]:0)%mod))%mod;
+                    curr[j]=(long)(i+1<m?next[j]:0)+(long)(j+1<n?curr[j+1]:0);
                 }
             }
             next=curr;
-            curr.resize(n,0);
+
         }
         return next[0];
 
