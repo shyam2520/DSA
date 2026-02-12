7e2750ff86091b1037f5ca56a0b9b47eb3e58fca
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-12 16:29:43 -0500

    Time: 0 ms (100%), Space: 12.7 MB (72.41%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-
-    int recursion(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>& dp){
-        if(i==triangle.size()-1){
-            return j<triangle[i].size()?triangle[i][j]:INT_MAX;
-        }
-        if(dp[i][j]!=INT_MAX) return dp[i][j];
-
-        int d = recursion(triangle,i+1,j,dp);
-        int dr = recursion(triangle,i+1,j+1,dp);
-        
-        return dp[i][j]=triangle[i][j]+min(d,dr);
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int  m=triangle.size();
-        // int n = 
-        vector<vector<int>> dp(m,vector<int>(m,INT_MAX));
-        return recursion(triangle,0,0,dp);
+        // vector<vector<int>> dp(m,vector<int>(m));
+        vector<int> curr(m);
+        vector<int> next(m);
+        for(int i=m-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                if(i==m-1) curr[j]=triangle[i][j];
+                else{
+                    curr[j]=triangle[i][j]+min(next[j],next[j+1]);
+                }
+            }
+            next=curr;
+            curr.resize(i);
+        }
+        return next[0];
     }
 };
