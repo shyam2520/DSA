536776edc0aca7319fe806cb16783d5e7c97948c
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-14 08:10:03 -0500

    Time: 39 ms (94.61%), Space: 111.1 MB (23.24%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
 
-    bool subsetExists(vector<int>& nums,int sum,int total,int idx,
-    vector<vector<int>>& dp){
-        if(sum==total-sum) return true;
+    bool subsetExists(vector<int>& nums,int idx,vector<vector<int>>& dp,int target){
+        if(!target) return true;
         if(idx==nums.size()) return false; 
-        if(dp[idx][sum]!=-1) return dp[idx][sum]?true:false;
-
-        dp[idx][sum] = subsetExists(nums,sum+nums[idx],total,idx+1,dp) ||
-        subsetExists(nums,sum,total,idx+1,dp);
-        return dp[idx][sum]?true:false;
+        if(dp[idx][target]!=-1) return dp[idx][target]?true:false;
+        if(nums[idx]<=target){
+            dp[idx][target] = subsetExists(nums,idx+1,dp,target-nums[idx]) || subsetExists(nums,idx+1,dp,target);
+        }
+        else{
+            dp[idx][target] = false;
+        }
+        return dp[idx][target];
     }
 
     bool canPartition(vector<int>& nums) {
         int total = 0;
         for(auto& i:nums) total+=i;
-        vector<vector<int>> dp(nums.size()+1,vector<int>(total+1,-1));
-        return subsetExists(nums,0,total,0,dp);
+        if(total%2) return false;
+        int target = total/2;
+        sort(begin(nums),end(nums));
+        vector<vector<int>> dp(nums.size()+1,vector<int>(target+1,-1));
+        return subsetExists(nums,0,dp,target);
     }
 };
