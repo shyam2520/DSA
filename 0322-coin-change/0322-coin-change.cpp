8c77272dc291fc28f7b133e8e618692f12dc0a13
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-16 09:19:47 -0500

    Time: 37 ms (49.02%), Space: 48.9 MB (40.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -13,9 +13,7 @@ public:
             int notake = coinCombs(coins,idx-1,amt,dp);
             return dp[idx][amt]=min(take,notake);
         }
-
         return dp[idx][amt]=coinCombs(coins,idx-1,amt,dp);
-
     }
 
     
@@ -23,8 +21,26 @@ public:
         // take or not take from high - low 
         sort(begin(coins),end(coins));
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        int ans =  coinCombs(coins,n-1,amount,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int i=1;i<=amount;i++){
+            if(i%coins[0]) dp[0][i]=1e8;
+            else dp[0][i]=i/coins[0];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                if(j>=coins[i]){
+                    int take = 1+dp[i][j-coins[i]];
+                    int notake = dp[i-1][j];
+                    dp[i][j]=min(take,notake);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+
+        
+        int ans =  dp[n-1][amount];
         return ans>=1e8?-1:ans;
     }
 
