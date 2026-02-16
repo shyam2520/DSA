def8c2b835fcf4aefb8fffaaad912f3e4a44721e
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-16 09:02:01 -0500

    Time: 35 ms (52.71%), Space: 53.1 MB (11.12%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int coinCombs(vector<int>& coins,int idx,int amt,vector<vector<int>>& dp){
+        if(!amt) return 0;
+        if(idx==0){
+            if(amt%coins[idx]!=0) return 1e8;
+            return amt/coins[idx];
+        }
+        if(dp[idx][amt]!=-1) return dp[idx][amt];
+
+        if(amt>=coins[idx]){
+            int take = 1+coinCombs(coins,idx,amt-coins[idx],dp);
+            int notake = coinCombs(coins,idx-1,amt,dp);
+            return dp[idx][amt]=min(take,notake);
+        }
+
+        return dp[idx][amt]=coinCombs(coins,idx-1,amt,dp);
+
+    }
+
+    
+    int coinChange(vector<int>& coins, int amount) {
+        // take or not take from high - low 
+        sort(begin(coins),end(coins));
+        int n = coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        int ans =  coinCombs(coins,n-1,amount,dp);
+        return ans>=1e8?-1:ans;
+    }
+
+
+};
