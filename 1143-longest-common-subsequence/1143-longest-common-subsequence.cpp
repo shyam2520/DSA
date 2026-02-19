a3636cdc81a68d257f40529a95dcbfe371d74387
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-19 08:32:53 -0500

    Time: 26 ms (66.33%), Space: 27.3 MB (68.68%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,7 +5,9 @@ public:
             return 0;
         }
 
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        } 
 
         if(s1[i]==s2[j]){
             return dp[i][j]=1+lcs(s1,s2,i+1,j+1,dp);
@@ -15,7 +17,19 @@ public:
 
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size(),n= text2.size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return lcs(text1,text2,0,0,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(text1[i]==text2[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                }
+
+            }
+        }
+        return dp[0][0];
+        // return lcs(text1,text2,0,0,dp);
     }
 };
