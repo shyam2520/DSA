37ecf65b2f8e6318908c0174f9b7035f7c12c05b
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-20 13:39:59 -0500

    Time: 34 ms (54.97%), Space: 31.5 MB (39.51%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,8 +3,8 @@ public:
     int recursion(string& s,int l,int r,vector<vector<int>>& dp){
         if(l>r) return 0;
         if(l==r) return 1; 
-        if(s[l]==s[r]) return 2+recursion(s,l+1,r-1,dp);
         if(dp[l][r]!=-1) return dp[l][r];
+        if(s[l]==s[r]) return 2+recursion(s,l+1,r-1,dp);
         return dp[l][r]=max(
             recursion(s,l+1,r,dp),
             recursion(s,l,r-1,dp)
@@ -14,9 +14,18 @@ public:
     int minInsertions(string s) {
         // find the longest pal subseq
         // n - longpal = remaining are palindrom 
-        int l=0,r=s.size();
-        vector<vector<int>> dp(r,vector<int>(r,-1));
-        int pal =  recursion(s,l,r-1,dp);
-        return r-pal;
+        int l=0,n=s.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=n;j++){
+        //         int l=i-1;
+        //         int r=j-1;
+        //         if(l==r) dp[i][j]=1;
+        //         else if(s[l]==s[r]) dp[i][j]=2+dp[]
+        //     }
+        // }
+
+        int pal =  recursion(s,l,n-1,dp);
+        return n-pal;
     }
 };
