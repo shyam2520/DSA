c26f98ef88fe3b0760271531b560a40facd83978
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-22 10:13:18 -0500

    Time: 8 ms (18.17%), Space: 24.6 MB (12.34%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -14,10 +14,24 @@ public:
                 prices[idx]+buysell(prices,idx+1,1,dp),
                 buysell(prices,idx+1,0,dp)
             );
+
         }
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return buysell(prices,0,1,dp);
+        int n = prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        // return buysell(prices,0,1,dp);
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                if(buy){
+                    dp[i][buy]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+                else{
+                    dp[i][buy]=max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                }
+            }
+        }
+
+        return dp[0][1];
     }
 };
