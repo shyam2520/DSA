232d96c24253c615bd4c1f3ec82a225f88713503
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-23 18:30:34 -0500

    Time: 15 ms (73.46%), Space: 24.1 MB (36.23%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,30 +1,29 @@
 class DisjointSet{
     private: 
-    vector<int> parent,size; 
-    public: 
+    vector<int> parent,size;
+    public:
     DisjointSet(int n){
-        size.resize(n ,1); 
-        for(int i=0;i<n;i++) parent.push_back(i);
+        size.resize(n,1);
+        for(int i=0;i<n;i++) parent.push_back(i); 
     }
-    
-    int getParent(int n){
-        if(parent[n]==n) return n; 
+    int getParent(int n ){
+        if(parent[n]==n) return n;
         return parent[n]=getParent(parent[n]);
     }
 
     void unionBySize(int u,int v){
-        int ultpu = getParent(u);
-        int ultpv = getParent(v); 
-        if(ultpu==ultpv) return ; 
-        int sizeu = size[ultpu];
-        int sizev = size[ultpv]; 
-        if(sizeu<sizev){
-            parent[ultpu]=ultpv;
-            size[ultpv]+=sizeu;
+        int ultp_u = getParent(u);
+        int ultp_v = getParent(v);
+        int sizeu = size[ultp_u];
+        int sizev= size[ultp_v];
+        if(ultp_u==ultp_v) return;
+        if(sizeu>sizev){
+            parent[ultp_v]=ultp_u;
+            size[ultp_u]+=sizev;
         }
         else{
-            parent[ultpv]=ultpu;
-            size[ultpu]+=sizev;
+            parent[ultp_u]=ultp_v;
+            size[ultp_v]+=sizeu;
         }
     }
 };
@@ -32,30 +31,33 @@ class DisjointSet{
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
-        // make the row & col two be different nodes 
-        int maxrow=0,maxcol=0;
-        for(auto& stone:stones){
-            int x = stone[0];
-            int y = stone[1]; 
-            maxrow=max(maxrow,x);
-            maxcol=max(maxcol,y);
+        // get the max rows , cols 
+        // ds size row+cols+1
+        //  unione of cordinat i,(rows+i)
+        int max_rows=0,max_cols=0;
+        for(auto& i:stones){
+            max_rows=max(max_rows,i[0]);
+            max_cols=max(max_cols,i[1]);
         }
-        DisjointSet ds = DisjointSet(maxrow+1+maxcol+1);
-        unordered_map<int,int> unqstones;
-        for(auto& stone:stones){
-            int x = stone[0];
-            int y = stone[1];
-            ds.unionBySize(x,maxrow+1+y);
-            unqstones[x]++;
-            unqstones[maxrow+1+y]++;
+        DisjointSet ds(max_rows+max_cols+2);
+        unordered_map<int,int> dict;
+        for(auto& i:stones){
+            int x=i[0],y=i[1];
+            y = max_rows+y+1;
+            dict[x]++;
+            dict[y]++;
+            int ultp_u = ds.getParent(x);
+            int ultp_v = ds.getParent(y);
+            if(ultp_u==ultp_v) continue;
+            ds.unionBySize(ultp_u,ultp_v);
         }
-        int cnt=0;
-        for(auto& i:unqstones){
-            if(ds.getParent(i.first)==i.first) cnt++;
+        int comp =0;
+        // 0 1 2 3
+        // 0 0 2 0
+        for(auto& i:dict){
+            if(i.first==ds.getParent(i.first)) comp++;
         }
-
-        return stones.size()-cnt;
-
+        return stones.size()-comp;
 
     }
 };
