a6a5a46fc3a8ab3c51fdf6b93bbd9708fb228614
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-02-27 21:03:36 -0500

    Time: 1267 ms (49.43%), Space: 9.5 MB (66.77%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int checkPalindrome(string& s,int i,int j){
+        while(i<j && s[i]==s[j]){
+            i++;j--;
+        }
+        return i>=j;
+    }
+    int genCuts(string& s,int start,vector<int>& dp){
+        if(start==s.length()) return 0;
+        if(dp[start]!=-1) return dp[start];
+        int cuts = 2100; 
+        for(int i=start;i<s.length();i++){
+            // start -> i 
+            if(checkPalindrome(s,start,i)){
+                cuts = min(cuts,1+genCuts(s,i+1,dp));
+            }
+        }
+        return dp[start]=cuts;
+    }
+
+    int minCut(string s) {
+        // aab 
+        vector<int> dp(s.length()+1,-1);
+        int strcnt= genCuts(s,0,dp);
+        return strcnt-1;
+    }
+};
