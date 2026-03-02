a76ad0a73b57ed724e34ddf7bb5adf967f620a4b
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-02 18:25:25 -0500

    Time: 0 ms (100%), Space: 8.3 MB (29.34%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     int minCost(int n) {
         int cost = 0;
-        priority_queue<int,vector<int>,greater<>> pq;
-        for(int i=0;i<n;i++) pq.push(1);
-        while(pq.size()>1){
-            int top1 = pq.top();
-            pq.pop();
-            int top2 = pq.top();
-            pq.pop();
-            cost+= top1*top2;
-            pq.push(top1+top2);
+        while(n!=1){
+            cost+=(n-1);
+            n--;
         }
         return cost;
+    
     }
 };
