b270296d65f3ab7b500ac5dbc000190d0f8d7338
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-02 18:28:24 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int minCost(int n) {
-        int cost = 0;
-        while(n!=1){
-            cost+=(n-1);
-            n--;
-        }
-        return cost;
+        // return ((n-1)*(n-3))/2;
+        return n*(n-1);
     
     }
 };
