20fbbfec41ec684cb33089a4881dd4c8db3a5fdb
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-03 19:18:30 -0500

    Time: 4 ms (94.1%), Space: 9.4 MB (62.61%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int res=0;
-    int st=-1,end=-1;
-    void checkPalindrome(string& s,int l,int r){
+    pair<int,int> checkPalindrome(string& s,int l,int r){
+        int prevl=-1,prevr=-1;
         while(l>=0 && r<s.length() && s[l]==s[r]){
-            l--;
-            r++;
+            prevl=l;
+            prevr=r;
+            l--;r++;
         }
-        
-        if((r-1)-(l+1)+1>res){
-            res=(r-1)-(l+1)+1;
-            end=r-1;
-            st=l+1;
-        }
+        return {prevl,prevr};
     }
-
     string longestPalindrome(string s) {
-        
-        for(int i=0;i<s.length();i++){
-            checkPalindrome(s,i,i);
-            checkPalindrome(s,i,i+1);
-        }
-
-        return s.substr(st,res);
+        // aba
+        // abba
+        int n = s.size(); 
+        int res = 0,l=-1,r=-1;
+        for(int i=0;i<n;i++)
+        {
+            auto [l1,r1]=checkPalindrome(s,i,i);
+            auto [l2,r2]= checkPalindrome(s,i,i+1);
+            int len1 = (r1-l1)+1;
+            int len2 = (r2-l2)+1;
+            if(len1>res){
+                res=len1;
+                l=l1;r=r1;
+            }
+            if(len2>res){
+                res=len2;
+                l=l2,r=r2;
+            }
+        } 
+        return s.substr(l,res);
     }
 };
