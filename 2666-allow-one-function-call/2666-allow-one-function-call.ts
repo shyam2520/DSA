70c658c0bf008aa4a7e9eeef17739d8231fe86be
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-05 14:37:11 -0500

    Time: 26 ms (99.86%), Space: 54 MB (91.55%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,24 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    
+    let flag:boolean=false;
+    return function (...args) {
+        if(!flag){
+            flag=true;
+            return fn(...args);
+        } 
+        else{
+            return undefined;
+        }
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
