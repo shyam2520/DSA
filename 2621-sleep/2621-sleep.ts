0e0d6f6c6baeedc219ad28b87f4f3fe72677329c
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-05 15:53:17 -0500

    Time: 42 ms (73.62%), Space: 53.2 MB (98.92%) - LeetHub

---
+++ 2621-sleep/2621-sleep.ts
@@ -0,0 +1,14 @@
+async function sleep(millis: number): Promise<void> {
+    return new Promise((resolve,reject)=>{
+        setTimeout(
+        (millis)=>resolve(millis),
+        millis);
+    
+    });
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
