f465c72443b4b7365a80c5c1e0725d96764a44b4
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-05 14:21:04 -0500

    Time: 75 ms (5.75%), Space: 56.9 MB (48.64%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.ts
@@ -0,0 +1,21 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    
+    return function(x) {
+        // console.log(x);
+        // return 0;
+        let res:any=x;
+        for(let i=functions.length-1;i>=0;i--){
+            if(res){
+                res = functions[i](res);
+            }
+        }
+        return res;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
