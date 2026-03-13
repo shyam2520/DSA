d8ae8c0b0f8e1bc7d838408225826b74375397aa
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-13 17:27:41 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> champ(101,vector<double>(101,0.0));
-        champ[0][0]=poured;
+        // vector<vector<double>> champ(101,vector<double>(101,0.0));
+        vector<double> curr(101,0.0);
+        vector<double> next(101,0.0);
+        curr[0]=poured;
         for(int i=0;i<=query_row;i++){
             for(int j=0;j<=i;j++){
-                double glass =min((double)1,champ[i][j]);
-                double rem = champ[i][j]-glass;
-                champ[i][j]=glass;
+                double glass =min(1.0,curr[j]);
+                double rem = curr[j]-glass;
+                curr[j]=glass;
                 double split = rem/2;
-                champ[i+1][j]+=max((double)0,split);
-                if((j+1)<champ[0].size()) champ[i+1][j+1]+=max(0.0,split);
+                next[j]+=max(0.0,split);
+                if((j+1)<next.size()) next[j+1]+=max(0.0,split);
             }
+            if(i==query_row) return curr[query_glass];
+            curr=next;
+            next=vector<double>(101,0.0);
         }
-        return champ[query_row][query_glass];
+        return curr[query_glass];
 
     }
 };
