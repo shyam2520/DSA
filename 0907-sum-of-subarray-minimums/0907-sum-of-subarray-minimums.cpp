75f3f9a2b63682c82adb41c3aebb366df511a684
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-23 08:15:30 -0400

    Time: 10 ms (89%), Space: 46.4 MB (89.38%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    int mod = 1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> dp(n,-1);
-        vector<int> nse(n,n);
+        // next smaller element 
         stack<int> stack;
+        int n = arr.size(),mod = 1e9+7;
+        vector<int> nse(n,n);
         for(int i=0;i<n;i++){
-            while(stack.size() && arr[stack.top()]>arr[i]){
+            while(stack.size() && arr[stack.top()]>arr[i]) {
                 nse[stack.top()]=i;
                 stack.pop();
             }
             stack.push(i);
         }
-        int res = 0;
+        vector<int> dp(n);int res=0;
         for(int i=n-1;i>=0;i--){
-            int nseidx = nse[i];
-            int sum_min = 0;
-            if(nseidx==n){
-                sum_min = ((arr[i]%mod)*((n-i)%mod))%mod;
-            }
-            else{
-                sum_min = ((arr[i]%mod)*((nseidx-i)%mod))%mod+(dp[nseidx])%mod;
-            }
-            dp[i] = sum_min;
-            res=res%mod + dp[i]%mod;
+            int curr = arr[i];
+            int nsev = nse[i];
+            // if()
+            dp[i]+=curr*(nsev-i);
+            if(nsev!=n) dp[i]+=dp[nsev];
+            res=(res%mod+(dp[i]%mod))%mod;
         }
-
         return res;
+
     }
 };
