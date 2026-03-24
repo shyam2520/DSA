11abc8f864e3aa17ce4447cc828cdc1f5d8537f6
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-24 18:17:51 -0400

    Time: 31 ms (92.39%), Space: 26 MB (40.25%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
-    int longestStrChain(vector<string>& words) {
-        
-        sort(begin(words),end(words),[](string& a,string& b){
-            return a.length()<b.length();
-        });
-        int ans=0;
-        unordered_map<string,int> dict;
-        for(auto& word:words)
-        {
-            int res=0;
-            for(int i=0;i<word.length();i++)
-            {
-                string temp=word.substr(0,i)+word.substr(i+1);
-                res=max(res,dict.find(temp)==dict.end()?1:dict[temp]+1);
-            }
-            dict[word]=res;
-            ans=max(ans,res);
+
+    bool wordcompare(string& a,string& b){
+        int anc=0,miss=0;
+        for(int i=0;i<b.size();i++){
+            if(a[anc]==b[i]) anc++;
+            else miss++;
         }
-        return ans;
+        return miss<2;
     }
+
+    int longestStrChain(vector<string>& words) {
+        // set(words)
+        //  a->  [ba,da,db] -> []
+        // n*n -> largest word * m 
+        unordered_map<int,vector<string>> len_d;
+        for(auto& i:words) len_d[i.size()].push_back(i);
+        unordered_map<string,int> dp;
+        for(auto& i:words){
+            dfs(i,len_d,dp);
+        }
+        int res = -1;
+        for(auto& i:dp) res=max(res,i.second);
+        return res;
+    }
+
+    int dfs(string pre,unordered_map<int,vector<string>>& dict,unordered_map<string,int>& dp){
+        int n = pre.size();
+        // if(dict.find(n+1)==dict.end()){
+        //     dp[pre]=1;
+        //     return 0;
+        // } 
+        if(dp.count(pre)) return dp[pre];
+        int res=1;
+        for(auto& wrd:dict[n+1]){
+            if(wordcompare(pre,wrd)){
+                res=max(res,1+dfs(wrd,dict,dp));
+            }
+        }
+        return dp[pre]=res;
+    }
+
 };
