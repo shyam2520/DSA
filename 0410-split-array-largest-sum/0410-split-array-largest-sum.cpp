2cb234d72b4fb8e357c9cccb1e1cb464707df3fc
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-03-24 13:50:37 -0400

    Time: 0 ms (100%), Space: 10.6 MB (31.71%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-    int get_subarray(vector<int>& nums,int max_sum){
-        int total = 0,sub_array=0;
-        for(int i=0;i<nums.size();i++){
-            if(total+nums[i]>max_sum){
-                sub_array++;
-                total=nums[i];
-            }
-            else total+=nums[i];
-        }
-        return sub_array;
-    }
-
-    int splitArray(vector<int>& nums, int k) {
-        int l=0,r=0,ans=-1,mid;
+    int getSplits(vector<int>& nums,int max_sum){
+        int total=0,splits=0;
         for(auto& i:nums){
-            l=max(i,l); 
-            r+=i;
+            if((i+total)>max_sum){
+                splits++;
+                total=i;
+            }
+            else total+=i;
         }
-        nums.push_back(r);
+        return splits+1;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        // we are trying to minimize the 
+        // the worse case sum would be entire sum of arry 
+        // best case sum would be the large value of 
+        // range of answers should be between this qualifies for splits 
+        int total = 0,max_val=0;
+        for(auto& i:nums){
+            total+=i;
+            max_val=max(max_val,i);
+        } 
+        int l=max_val,r=total;
+        int res = -1;
         while(l<=r){
-            mid=l+(r-l)/2;
-            if(get_subarray(nums,mid)>k) l=mid+1; 
-            else {
-                ans=mid;
+            int mid = l+(r-l)/2;
+            // cout<<mid<<endl;
+            if(getSplits(nums,mid)>k){
+                l=mid+1;
+            }
+            else{
+                res=mid;
                 r=mid-1;
             }
         }
+        return res;
 
-        return ans;
     }
 };
