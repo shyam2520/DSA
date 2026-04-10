f64d5938fa5cbf91d1b72f4e4c900d0cebf23c86
Shyam Sundar Mylai <shyam2562000@gmail.com>
2026-04-10 19:29:34 -0400

    Time: 4 ms (64.31%), Space: 19.2 MB (67.43%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adjMat,int course,
-            vector<int>& visited,vector<int>& pathvisited)
-    {
-        visited[course]=1;
-        pathvisited[course]=1;
-        for(auto& n:adjMat[course]){
-            if(visited[n]!=-1){
-                if(pathvisited[n]==1) return true;
-            }
-            else{
-                if(dfs(adjMat,n,visited,pathvisited)) return true; 
-            }
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjMat(n);
+        vector<int> indegree(n);
+        for(auto& i:prerequisites){
+            int a = i[0];
+            int b = i[1];
+            adjMat[a].push_back(b);
+            indegree[b]++;
         }
-        pathvisited[course]=-1;
-        return false;
-    }
-
-    bool canFinish(int n, vector<vector<int>>& prereq) {
-        // 1. Detect loop using DFS 
-        // 2. its would be the same thing how we do normal dfs 
-        // 3. we have an addition of path visited because to different path can 
-        // end up at the same node doesn't need to form a cycle 
-        // 4. so Ideally only if an node is visited & path visited then it is valid
-        // 5. during back tracking set pathvisited back to 0 or -1 
-        vector<int> visited(n,-1),pathvisited(n,-1); 
-        vector<vector<int>> adjMat(n); 
-
-        for(auto& course:prereq){
-            int c1 = course[0]; 
-            int c2 = course[1]; 
-            adjMat[c2].push_back(c1);
-        }
-
-        for(int i=0;i<n;i++){
-            if(visited[i]==-1){
-                bool res = dfs(adjMat,i,visited,pathvisited); 
-                if(res) return false;
+        queue<int> q; 
+        for(int i=0;i<n;i++) if(!indegree[i]) q.push(i);
+        while(q.size()){
+            int course = q.front();
+            q.pop();
+            for(auto& neigh:adjMat[course]){
+                indegree[neigh]--;
+                if(!indegree[neigh]) q.push(neigh);
             }
         }
-        return true; 
+        for(auto& i:indegree) if(i) return false;
+        return true;
     }
 };
